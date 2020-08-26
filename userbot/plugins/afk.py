08572d30a401db56e2c132b30c4d9021f4ff5b1b
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-08-27 01:32:43 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -155,7 +155,7 @@ async def _(event):
             logger.warn(str(e))  # pylint:disable=E0602
 
 CMD_HELP.update({
-    "afk":"__**PLUGIN NAME :** Alive__\
+    "afk":"__**PLUGIN NAME :** Afk__\
 \n\n📌** CMD ➥** `.afk` [Optional Reason]\
 \n**USAGE   ➥  **Sets you as afk.\nReplies to anyone who tags/PM's \
 you telling them that you are AFK(reason)\n\n__Switches off AFK when you type back anything, anywhere.__\
