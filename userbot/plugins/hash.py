2ac0879d326e37ccb8ca8b0d9af5941386c12dc4
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-08-30 13:05:14 +0530

    Update hash.py

--- userbot/plugins/hash.py
+++ userbot/plugins/hash.py
@@ -1,195 +1,73 @@
-"""CC- @refundisillegal\nSyntax:-\n.get var NAME\n.del var NAME\n.set var NAME"""
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot module containing hash and encode/decode commands. """
 
-# Copyright (C) 2020 Adek Maulana.
-# All rights reserved.
-"""
-   Heroku manager for your userbot
-"""
-import os
-import math
-import heroku3
-import asyncio
-import urllib3
-import requests
-from .. import CMD_HELP
-from ..utils import admin_cmd, sudo_cmd, edit_or_reply
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-# ================= 
-
-Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
-Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
-heroku_api = "https://api.heroku.com"
-HEROKU_APP_NAME = Config.HEROKU_APP_NAME
-HEROKU_API_KEY = Config.HEROKU_API_KEY
-
-Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
-heroku_api = "https://api.heroku.com"
+from subprocess import PIPE
+from subprocess import run as runapp
+import pybase64
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd, errors_handler
 
 
-@borg.on(admin_cmd(pattern="(set|get|del) var(?: |$)(.*)(?: |$)([\s\S]*)", outgoing=True))
-@borg.on(sudo_cmd(pattern="(set|get|del) var(?: |$)(.*)(?: |$)([\s\S]*)",allow_sudo = True))
-async def variable(var):
-    """
-        Manage most of ConfigVars setting, set new var, get current var,
-        or delete var...
-    """
-    if Var.HEROKU_APP_NAME is not None:
-        app = Heroku.app(Var.HEROKU_APP_NAME)
+@borg.on(admin_cmd(outgoing=True, pattern="hash (.*)"))
+@errors_handler
+async def gethash(hash_q):
+    """ For .hash command, find the md5, sha1, sha256, sha512 of the string. """
+    hashtxt_ = hash_q.pattern_match.group(1)
+    hashtxt = open("hashdis.txt", "w+")
+    hashtxt.write(hashtxt_)
+    hashtxt.close()
+    md5 = runapp(["md5sum", "hashdis.txt"], stdout=PIPE)
+    md5 = md5.stdout.decode()
+    sha1 = runapp(["sha1sum", "hashdis.txt"], stdout=PIPE)
+    sha1 = sha1.stdout.decode()
+    sha256 = runapp(["sha256sum", "hashdis.txt"], stdout=PIPE)
+    sha256 = sha256.stdout.decode()
+    sha512 = runapp(["sha512sum", "hashdis.txt"], stdout=PIPE)
+    runapp(["rm", "hashdis.txt"], stdout=PIPE)
+    sha512 = sha512.stdout.decode()
+    ans = ("Text: `" + hashtxt_ + "`\nMD5: `" + md5 + "`SHA1: `" + sha1 +
+           "`SHA256: `" + sha256 + "`SHA512: `" + sha512[:-1] + "`")
+    if len(ans) > 4096:
+        hashfile = open("hashes.txt", "w+")
+        hashfile.write(ans)
+        hashfile.close()
+        await hash_q.client.send_file(
+            hash_q.chat_id,
+            "hashes.txt",
+            reply_to=hash_q.id,
+            caption="`It's too big, sending a text file instead. `")
+        runapp(["rm", "hashes.txt"], stdout=PIPE)
     else:
-        return await edit_or_reply(var ,"`[HEROKU]:"
-                              "\nPlease setup your` **HEROKU_APP_NAME**")
-    exe = var.pattern_match.group(1)
-    heroku_var = app.config()
-    if exe == "get":
-        cat = await edit_or_reply(var ,"`Getting information...`")
-        await asyncio.sleep(1.0)
-        try:
-            variable = var.pattern_match.group(2).split()[0]
-            if variable in heroku_var:
-                return await cat.edit("**ConfigVars**:"
-                                      f"\n\n`{variable} = {heroku_var[variable]}`\n")
-            return await cat.edit("**ConfigVars**:"
-                                      f"\n\n`Error:\n-> {variable} don't exists`")
-        except IndexError:
-            configs = prettyjson(heroku_var.to_dict(), indent=2)
-            with open("configs.json", "w") as fp:
-                fp.write(configs)
-            with open("configs.json", "r") as fp:
-                result = fp.read()
-                if len(result) >= 4096:
-                    await bot.send_file(
-                        var.chat_id,
-                        "configs.json",
-                        reply_to=var.id,
-                        caption="`Output too large, sending it as a file`",
-                    )
-                else:
-                    await cat.edit("`[HEROKU]` ConfigVars:\n\n"
-                                   "================================"
-                                   f"\n```{result}```\n"
-                                   "================================"
-                                   )
-            os.remove("configs.json")
-            return
-    elif exe == "set":
-        cat = await edit_or_reply(var ,"`Setting information...`")
-        variable = var.pattern_match.group(2)
-        if not variable:
-            return await cat.edit(">`.set var <ConfigVars-name> <value>`")
-        value = var.pattern_match.group(3)
-        if not value:
-            variable = variable.split()[0]
-            try:
-                value = var.pattern_match.group(2).split(' ', 1)[1]
-            except IndexError:
-                return await cat.edit(">`.set var <ConfigVars-name> <value>`")
-        await asyncio.sleep(1.5)
-        if variable in heroku_var:
-            await cat.edit(f"`{variable}` **successfully changed to  ->  **`{value}`")
-        else:
-            await cat.edit(f"`{variable}`**  successfully added with value`  ->  **{value}`")
-        heroku_var[variable] = value
-    elif exe == "del":
-        cat = await edit_or_reply(var ,"`Getting information to deleting variable...`")
-        try:
-            variable = var.pattern_match.group(2).split()[0]
-        except IndexError:
-            return await cat.edit("`Please specify ConfigVars you want to delete`")
-        await asyncio.sleep(1.5)
-        if variable in heroku_var:
-            await cat.edit(f"`{variable}`  **successfully deleted**")
-            del heroku_var[variable]
-        else:
-            return await cat.edit(f"`{variable}`**  is not exists**")
+        await hash_q.reply(ans)
 
-@borg.on(admin_cmd(pattern="usage$", outgoing=True))
-@borg.on(sudo_cmd(pattern="usage$",allow_sudo = True))
-async def dyno_usage(dyno):
-    """
-        Get your account Dyno Usage
-    """
-    dyno = await edit_or_reply(dyno ,"`Processing...`")
-    useragent = ('Mozilla/5.0 (Linux; Android 10; SM-G975F) '
-                 'AppleWebKit/537.36 (KHTML, like Gecko) '
-                 'Chrome/80.0.3987.149 Mobile Safari/537.36'
-                 )
-    user_id = Heroku.account().id
-    headers = {
-     'User-Agent': useragent,
-     'Authorization': f'Bearer {Var.HEROKU_API_KEY}',
-     'Accept': 'application/vnd.heroku+json; version=3.account-quotas',
-    }
-    path = "/accounts/" + user_id + "/actions/get-quota"
-    r = requests.get(heroku_api + path, headers=headers)
-    if r.status_code != 200:
-        return await dyno.edit("`Error: something bad happened`\n\n"
-                               f">.`{r.reason}`\n")
-    result = r.json()
-    quota = result['account_quota']
-    quota_used = result['quota_used']
 
-    """ - Used - """
-    remaining_quota = quota - quota_used
-    percentage = math.floor(remaining_quota / quota * 100)
-    minutes_remaining = remaining_quota / 60
-    hours = math.floor(minutes_remaining / 60)
-    minutes = math.floor(minutes_remaining % 60)
-    """ - Current - """
-    App = result['apps']
-    try:
-        App[0]['quota_used']
-    except IndexError:
-        AppQuotaUsed = 0
-        AppPercentage = 0
+@borg.on(admin_cmd(outgoing=True, pattern="hbase (en|de) (.*)"))
+@errors_handler
+async def endecrypt(query):
+    """ For .base64 command, find the base64 encoding of the given string. """
+    if query.pattern_match.group(1) == "en":
+        lething = str(
+            pybase64.b64encode(bytes(query.pattern_match.group(2),
+                                     "utf-8")))[2:]
+        await query.reply("Shhh! It's Encoded: `" + lething[:-1] + "`")
     else:
-        AppQuotaUsed = App[0]['quota_used'] / 60
-        AppPercentage = math.floor(App[0]['quota_used'] * 100 / quota)
-    AppHours = math.floor(AppQuotaUsed / 60)
-    AppMinutes = math.floor(AppQuotaUsed % 60)
-    await asyncio.sleep(1.5)
-    return await dyno.edit("**Dyno Usage**:\n\n"
-                           f" -> `Dyno usage for`  **{Var.HEROKU_APP_NAME}**:\n"
-                           f"     •  `{AppHours}`**h**  `{AppMinutes}`**m**  "
-                           f"**|**  [`{AppPercentage}`**%**]"
-                           "\n\n"
-                           " -> `Dyno hours quota remaining this month`:\n"
-                           f"     •  `{hours}`**h**  `{minutes}`**m**  "
-                           f"**|**  [`{percentage}`**%**]"
-                           )
+        lething = str(
+            pybase64.b64decode(bytes(query.pattern_match.group(2), "utf-8"),
+                               validate=True))[2:]
+        await query.reply("Decoded: `" + lething[:-1] + "`")
 
-@borg.on(admin_cmd(pattern="herokulogs$", outgoing=True))
-@borg.on(sudo_cmd(pattern="herokulogs$",allow_sudo = True))
-async def _(dyno):        
-    try:
-        Heroku = heroku3.from_key(HEROKU_API_KEY)                         
-        app = Heroku.app(HEROKU_APP_NAME)
-    except:
-  	     return await dyno.reply(" Please make sure your Heroku API Key, Your App name are configured correctly in the heroku") 
-    data = app.get_log()
-    key = requests.post('https://nekobin.com/api/documents', json={"content": data}).json().get('result').get('key')
-    url = f'https://nekobin.com/{key}'
-    reply_text = f'Recent 100 lines of heroku logs: [here]({url})'
-    cat = await edit_or_reply(dyno ,reply_text)
-    
-def prettyjson(obj, indent=2, maxlinelength=80):
-    """Renders JSON content with indentation and line splits/concatenations to fit maxlinelength.
-    Only dicts, lists and basic types are supported"""
-    items, _ = getsubitems(obj, itemkey="", islast=True, maxlinelength=maxlinelength - indent, indent=indent)
-    return indentitems(items, indent, level=0)
 
-CMD_HELP.update({
-  "heroku":"__**PLUGIN NAME :** Alive__\
-  \n\n📌** CMD ➥** `.usage`\
-  \n**USAGE   ➥  **Check your heroku dyno hours status.\
-  \n\n📌** CMD ➥** `.set var` <NEW VAR> <VALUE>\
-  \n**USAGE   ➥  **Add new variable or update existing value variable\
-  \n\n📌** CMD ➥** `.get var` or `.get var <VAR>`\
-  \n**USAGE   ➥  **Get your existing varibles & valus of that\
-  \n\n📌** CMD ➥** `.del var` <VAR>\
-  \n**USAGE   ➥  **Delete existing variable\
-  \n\n📌** CMD ➥** `.herokulogs`\
-  \n**USAGE   ➥  **Sends you recent 100 lines of logs in heroku\
-  \n\n\n**!!! WARNING !!!, After adding or deleting variable the bot will restarted**\
-  \n**Don't use .get var in public groups.This returns all of your private information, please be cautious...**"
-})    
+CMD_HELP.update({"hash": ".hbase en or .hbase de \nUsage: Find the base64 encoding of the given string\
+    \n\n.hash\nUsage: Find the md5, sha1, sha256, sha512 of the string when written into a txt file."
+})
+
+CMD_HELP.update({"hash":"__**PLUGIN NAME :** Hash__\
+\n\n📌** CMD ➥** `.hbase en` or `.hbase de`\
+\n**USAGE   ➥  **Find the base64 encoding of the given string\
+\n\n📌** CMD ➥** `.hash`\
+\n**USAGE   ➥  **Find the md5, sha1, sha256, sha512 of the string when written into a txt file."
+})
