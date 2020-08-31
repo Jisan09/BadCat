ba41ecbdade71624f0b071d38a579716add08f06
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-08-31 09:26:47 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -3,7 +3,7 @@ import requests
 import os
 from userbot.uniborgConfig import Config
 import re 
-from userbot import StartTime
+
 
 if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
      os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
@@ -42,5 +42,5 @@ def check(cat):
     if hi:
         return True
     return False
-uptime = catdef.get_readable_time((time.time() - StartTime))
-statstext = f"**✧ Uptime :** `{uptime}\n`" 
+
+statstext = " Nothing Now"
