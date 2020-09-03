416541554a9848d1f17d18e6757cb3b7bbb6fc31
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-04 02:14:42 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,9 +1,8 @@
-from userbot import catdef
+from ..helpers import *
 import requests
 import os
 from userbot.uniborgConfig import Config
 import re
-
 
 if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
     os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
@@ -11,29 +10,6 @@ thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "thumb_image.jpg"
 # thumb image
 with open(thumb_image_path, "wb") as f:
     f.write(requests.get(Config.THUMB_IMAGE).content)
-
-deEmojify = catdef.deEmojify
-trumptweet = catdef.trumptweet
-changemymind = catdef.changemymind
-kannagen = catdef.kannagen
-moditweet = catdef.moditweet
-tweets = catdef.tweets
-waifutxt = catdef.waifutxt
-catmusic = catdef.catmusic
-catmusicvideo = catdef.catmusicvideo
-admin_groups = catdef.admin_groups
-iphonex = catdef.iphonex
-baguette = catdef.baguette
-threats = catdef.threats
-lolice = catdef.lolice
-trash = catdef.trash
-awooify = catdef.awooify
-convert_toimage = catdef.convert_toimage
-trap = catdef.trap
-phcomment = catdef.phcomment
-extract_time = catdef.extract_time
-take_screen_shot = catdef.take_screen_shot
-runcmd = catdef.runcmd
 
 
 def check(cat):
@@ -45,4 +21,4 @@ def check(cat):
     return False
 
 
-statstext = " Nothing Now"
+statstext = "yet to write"
