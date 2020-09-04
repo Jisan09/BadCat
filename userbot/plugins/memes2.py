494591b26d841e718c7f89c5d7c9b65384dacba0
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-04 18:51:11 +0530

    Update memes2.py

--- userbot/plugins/memes2.py
+++ userbot/plugins/memes2.py
@@ -244,22 +244,3 @@ async def payf(event):
         paytext * 8,
         paytext * 8)
     await event.edit(pay)
-
-
-@borg.on(admin_cmd(pattern="ctext ?(.*)"))
-async def payf(event):
-    paytext = event.pattern_match.group(1)
-    pay = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(
-        paytext * 8,
-        paytext * 8,
-        paytext * 2,
-        paytext * 2,
-        paytext * 2,
-        paytext * 2,
-        paytext * 2,
-        paytext * 2,
-        paytext * 2,
-        paytext * 2,
-        paytext * 8,
-        paytext * 8)
-    await event.edit(pay)
