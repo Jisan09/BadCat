cfcf306b07a617f722d148a9e71fac36c4ccb51e
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-04 18:02:39 +0530

    Update button.py

--- userbot/plugins/button.py
+++ userbot/plugins/button.py
@@ -6,7 +6,7 @@ import os
 import re
 from .. import CMD_HELP
 from telethon import Button
-from ..utils import admin_cmd, sudo_cmd
+from ..utils import admin_cmd, sudo_cmd, edit_or_reply
 
 # regex obtained from:
 # https://github.com/PaulSonOfLars/tgbot/blob/master/tg_bot/modules/helper_funcs/string_handling.py#L23
@@ -71,6 +71,34 @@ async def _(event):
 # Helpers
 
 
+@borg.on(admin_cmd(pattern=r"ibutton(?: |$)(.*)", outgoing=True))
+@borg.on(sudo_cmd(pattern="ibutton(?: |$)(.*)", allow_sudo=True))
+async def _(event):
+    reply_to_id = None
+    catinput = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    await event.get_reply_message()
+    # soon will try to add media support
+    if not catinput:
+        catinput = (await event.get_reply_message()).text
+    if not catinput:
+        await edit_or_reply(event, "`Give me some thing to write in bot inline`")
+        return
+    catinput = "Inline buttons " + catinput
+    tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
+    results = await bot.inline_query(
+        tgbotusername,
+        catinput
+    )
+    await results[0].click(
+        event.chat_id,
+        reply_to=reply_to_id,
+        hide_via=True
+    )
+    await event.delete()
+
+
 def build_keyboard(buttons):
     keyb = []
     for btn in buttons:
@@ -82,9 +110,23 @@ def build_keyboard(buttons):
 
 
 CMD_HELP.update({
+    "button":
+    "**Plugin : **`button`\
+    \n\n**SYNTAX : **`.cbutton`\
+    \n**USAGE :** Buttons must be in th format as [name on button]<buttonurl:link you want to open> and markdown is Default to html\
+    \n**EXAMPLE :** `.cbutton test [google]<buttonurl:https://www.google.com> [catuserbot]<buttonurl:https://t.me/catuserbot17:same> [support]<buttonurl:https://t.me/catuserbot_support>`\
+    \n\n**SYNTAX : **`.ibutton`\
+    \n**USAGE :** Buttons must be in th format as [name on button]<buttonurl:link you want to open>\
+    \n**EXAMPLE :** `.ibutton test [google]<buttonurl:https://www.google.com> [catuserbot]<buttonurl:https://t.me/catuserbot17:same> [support]<buttonurl:https://t.me/catuserbot_support>`\
+    "
+})
+CMD_HELP.update({
     "button": "__**PLUGIN NAME :** Button__\
     \n\n📌** CMD ➥** `.cbutton`\
     \n**USAGE   ➥  **Buttons must be in th format as [name on button]<buttonurl:link you want to open> and narddown is Default to html\
     \n\n**EXAMPLE :** `.cbutton test [google]<buttonurl:https://www.google.com> [catuserbot]<buttonurl:https://t.me/catuserbot17:same> [support]<buttonurl:https://t.me/catuserbot_support>`\
+    \n\n📌** CMD ➥** `.ibutton`\
+    \n**USAGE   ➥  **Buttons must be in th format as [name on button]<buttonurl:link you want to open>\
+    \n\n**EXAMPLE :** `.ibutton test [google]<buttonurl:https://www.google.com> [catuserbot]<buttonurl:https://t.me/catuserbot17:same> [support]<buttonurl:https://t.me/catuserbot_support>`\
     "
 })
