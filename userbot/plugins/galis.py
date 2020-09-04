c3099073aac6cc6cefd9a379935cbac247aa16c1
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-04 18:43:27 +0530

    Update and rename gaali.py to galis.py

--- userbot/plugins/galis.py
+++ userbot/plugins/galis.py
@@ -1,3 +1,5 @@
+#Created By @Jisan7509
+
 from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 import asyncio
@@ -27,7 +29,32 @@ async def _(event):
         await asyncio.sleep(animation_interval)
         await event.edit(animation_chars[i % 11])
 
+@borg.on(admin_cmd(pattern="fk (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    name = event.pattern_match.group(1)
+    animation_interval = 3
+    animation_ttl = range(0, 11)
+    await event.edit("👁👁")
+    animation_chars = [
 
+        f"👁👁\n  👄  =====> Abey {name} Chutiya",
+        f"👁👁\n  👅  =====> Abey {name} Gay",
+        f"👁👁\n  💋  =====> Abey {name} Lodu",
+        f"👁👁\n  👄  =====> Abey {name} Gandu",
+        f"👁👁\n  💋  =====> Abey {name} Randi",
+        f"👁👁\n  👄  =====> Abey {name} Betichod",
+        f"👁👁\n  👅  =====> Abey {name} Behenchod",
+        f"👁👁\n  💋  =====> Abey {name} NaMard",
+        f"👁👁\n  👄  =====> Abey {name} Lavde",
+        f"👁👁\n  👅  =====> Abey {name} Bhosdk",
+        f"👁👁\n  👄  =====> Hi {name} Mc, How Are You Bsdk..."
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 11])
+        
 @borg.on(admin_cmd(pattern=f"chod$", outgoing=True))
 async def _(event):
     if event.fwd_from:
@@ -76,7 +103,7 @@ async def _(event):
         await event.edit(animation_chars[i % 11])
 
 
-@borg.on(admin_cmd(pattern=f"virus$", outgoing=True))
+@borg.on(admin_cmd(pattern=f"quickheal$", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
