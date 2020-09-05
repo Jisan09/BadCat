a83d1c208f4e60eddc02b86e804b297b454e2d20
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-05 22:30:18 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -106,7 +106,7 @@ async def catalive():
         dyno = f"{AppHours}h {AppMinutes}m/{hours}h {minutes}m"
     except Exception as e:
         dyno = e
-    conclusion = f"🖤༄ Catuserbot Stats ༄🖤\
+    conclusion = f"♥️༄ Catuserbot Stats ༄♥️\
                  \n\nღ Database : {check_sgnirts}\
                   \nღ Sudo : {sudo}\
                   \nღ Uptime : {uptime}\
