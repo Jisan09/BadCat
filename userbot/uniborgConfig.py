76d6f9fd26f7cfe3ca03f5bd8c2d67f4a40637a5
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-10 13:31:11 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -35,7 +35,10 @@ class Config(object):
     IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
     IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
     # This is required for the @telegraph functionality.
-    TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "mrconfused")
+    TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "jisan")
+    # Set False to stop deleting old welcome messages
+    CLEAN_WELCOME = os.environ.get("CLEAN_WELCOME", False)
+    # github vars
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
     GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
     # Get a Free API Key from OCR.Space
