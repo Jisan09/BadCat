060f0917048d1762356c92ee2c4a7db0754cd816
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-11 23:48:50 +0530

    Update lydia.py

--- userbot/plugins/lydia.py
+++ userbot/plugins/lydia.py
@@ -111,7 +111,7 @@ async def on_new_message(event):
                 async with event.client.action(event.chat_id, "location"):
                     await asyncio.sleep(5)
                     output = lydia.think_thought(session_id, query)
-                    await event.reply("💫" + output)
+                    await event.reply(output)
             except cf.exception.CoffeeHouseError as e:
                 logger.info(str(e))
 
