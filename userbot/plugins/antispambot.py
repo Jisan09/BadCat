7afd209a5a35319be3b94a54f595ccd2aec10583
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-11 21:31:47 +0530

    Create antispambot.py

---
+++ userbot/plugins/antispambot.py
@@ -0,0 +1,25 @@
+from telethon import events
+
+from .. import LOGS
+from ..utils import is_admin
+from . import spamwatch
+
+if Config.SPAMWATCH_BAN and spamwatch:
+
+    @bot.on(events.ChatAction())
+    async def _(event):
+        chat = event.chat_id
+        if event.user_joined:
+            user = await event.get_user()
+            catadmin = await is_admin(bot, chat, bot.uid)
+            if not catadmin:
+                return
+            ban = spamwatch.get_ban(user.id)
+            if ban:
+                await event.reply(
+                    f"This [{user.first_name}](tg://user?id={user.id}) was banned by spamwatch For the reason `{ban.reason}`"
+                )
+                try:
+                    await bot.edit_permissions(chat, user.id, view_messages=False)
+                except Exception as e:
+                    return LOGS.info(e)
