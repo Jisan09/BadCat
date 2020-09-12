c7e4d32355e681f1980b7f73eb6bed18ce147748
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-13 01:01:12 +0530

    Update animation4.py

--- userbot/plugins/animation4.py
+++ userbot/plugins/animation4.py
@@ -8,34 +8,6 @@ DEF = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 USR = str(Config.LIVE_USERNAME) if Config.LIVE_USERNAME else "@Jisan7509"
 
 
-@borg.on(admin_cmd(pattern="eye$"))
-async def _(event):
-    if event.fwd_from:
-        return
-    animation_interval = 3
-    animation_ttl = range(0, 10)
-    # input_str = event.pattern_match.group(1)
-    # if input_str == "eye":
-    await event.edit("👁👁")
-    animation_chars = [
-        "👁👁\n  👄  =====> Hey, How are you?",
-        "👁👁\n  👅  =====> Everything okay?",
-        "👁👁\n  💋  =====> Why are you staring at this?",
-        "👁👁\n  👄  =====> You idiot",
-        "👁👁\n  👅  =====> Go away",
-        "👁👁\n  💋  =====> Stop laughing",
-        "👁👁\n  👄  =====> It's not funny",
-        "👁👁\n  👅  =====> I guess ur still looking",
-        "👁👁\n  💋  =====> Ok man 😑",
-        "👁👁\n  👄  =====> I go away then",
-    ]
-    for i in animation_ttl:
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 10])
-    await asyncio.sleep(animation_interval)
-    await event.delete()
-
-
 @borg.on(admin_cmd(pattern="kilr (.*)"))
 async def _(event):
     name = event.pattern_match.group(1)
