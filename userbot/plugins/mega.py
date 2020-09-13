edecb43b5a61651c50ccc700412176e36c162800
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-13 14:10:34 +0530

    Update and rename megadownloader.py to mega.py

--- userbot/plugins/mega.py
+++ userbot/plugins/mega.py
@@ -186,7 +186,7 @@ async def decrypt_file(megadl, file_path, temp_file_path, hex_key, hex_raw_key):
 
 CMD_HELP.update(
     {
-        "mega": "__**PLUGIN NAME :** Megadownloader__\
+        "mega": "__**PLUGIN NAME :** Mega__\
         \n\n📌** CMD ➥** `.mega` <MEGA.nz link>\
         \n**USAGE   ➥  **Reply to a MEGA.nz link or paste your MEGA.nz link\
         \n\n__ It will download the file into your userbot server.__"
