89587fed7adadb8d07f9607e3eb8d3083636f18d
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-23 02:09:54 +0530

    Update emojitxt.py

--- userbot/plugins/emojitxt.py
+++ userbot/plugins/emojitxt.py
@@ -3,7 +3,7 @@ from userbot.utils import admin_cmd
 from . import *
 
 
-@borg.on(admin_cmd(pattern="jisan(?: |$)(.*)"))
+@borg.on(admin_cmd(pattern="emoji(?: |$)(.*)"))
 async def itachi(event):
     args = event.pattern_match.group(1)
     if not args:
@@ -16,5 +16,23 @@ async def itachi(event):
     for chutiya in string:
         if chutiya in emojify.kakashitext:
             bsdk = emojify.kakashiemoji[emojify.kakashitext.index(chutiya)]
+            string = string.replace(chutiya, bsdk)
+    await event.edit(string)
+
+    
+@borg.on(admin_cmd(pattern="cemoji(?: |$)(.*)"))
+async def itachi(event):
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await event.edit("`What I am Supposed to do nibba `")
+        return
+    emoji , args = args.split(" ", 1)
+    string = "  ".join(args).lower()
+    for chutiya in string:
+        if chutiya in emojify.itachitext:
+            bsdk = emojify.itachiemoji[emojify.itachitext.index(chutiya)].format(cj = emoji)
             string = string.replace(chutiya, bsdk)
     await event.edit(string)
