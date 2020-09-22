52e3f81ef6047d7c1f4c9241214bb5c2dc8ae55d
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-23 03:52:07 +0530

    Update emojify.py

--- userbot/plugins/emojify.py
+++ userbot/plugins/emojify.py
@@ -4,36 +4,38 @@ Peru helper @mrconfused
 Userbot plugin for CatUserbot
 """
 from userbot import CMD_HELP
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 from . import *
 
 
 @borg.on(admin_cmd(pattern="emoji(?: |$)(.*)"))
+@borg.on(sudo_cmd(pattern="emoji(?: |$)(.*)", allow_sudo=True))
 async def itachi(event):
     args = event.pattern_match.group(1)
     if not args:
         get = await event.get_reply_message()
         args = get.text
     if not args:
-        await event.edit("`What I am Supposed to do nibba `")
+        await edit_or_reply(event,"`What am I Supposed to do with this nibba/nibbi, Give me a text. `")
         return
     string = "  ".join(args).lower()
     for chutiya in string:
         if chutiya in emojify.kakashitext:
             bsdk = emojify.kakashiemoji[emojify.kakashitext.index(chutiya)]
             string = string.replace(chutiya, bsdk)
-    await event.edit(string)
+    await edit_or_reply(event,string)
 
 
 @borg.on(admin_cmd(pattern="cmoji(?: |$)(.*)"))
+@borg.on(sudo_cmd(pattern="cmoji(?: |$)(.*)", allow_sudo=True))
 async def itachi(event):
     args = event.pattern_match.group(1)
     if not args:
         get = await event.get_reply_message()
         args = get.text
     if not args:
-        await event.edit("`What I am Supposed to do nibba `")
+        await edit_or_reply(event,"`What am I Supposed to do with this nibba/nibbi, Give me a text. `")
         return
     emoji, args = args.split(" ", 1)
     string = "  ".join(args).lower()
@@ -43,7 +45,7 @@ async def itachi(event):
                 cj=emoji
             )
             string = string.replace(chutiya, bsdk)
-    await event.edit(string)
+    await edit_or_reply(event,string)
 
 
 CMD_HELP.update(
