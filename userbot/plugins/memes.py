f2312cb27fcf2d36a30be2e04605183425656437
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-30 13:24:08 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -230,29 +230,6 @@ async def fuckedd(abusehard):
     await abusehard.edit(reply_text)
 
 
-RUNSREACTS = [
-    "`Congratulations and BRAVO!`",
-    "`You did it! So proud of you!`",
-    "`This calls for celebrating! Congratulations!`",
-    "`I knew it was only a matter of time. Well done!`",
-    "`Congratulations on your well-deserved success.`",
-    "`Heartfelt congratulations to you.`",
-    "`Warmest congratulations on your achievement.`",
-    "`Congratulations and best wishes for your next adventure!”`",
-    "`So pleased to see you accomplishing great things.`",
-    "`Feeling so much joy for you today. What an impressive achievement!`",
-]
-
-
-@borg.on(admin_cmd(pattern="congo"))
-async def _(event):
-    if event.fwd_from:
-        return
-    bro = random.randint(0, len(RUNSREACTS) - 1)
-    reply_text = RUNSREACTS[bro]
-    await event.edit(reply_text)
-
-
 @borg.on(admin_cmd(outgoing=True, pattern="abuse$"))
 async def abusing(abused):
     index = random.randint(0, len(memes.ABUSE_STRINGS) - 1)
@@ -483,7 +460,7 @@ async def bluetext(bt_e):
         )
 
 
-@borg.on(admin_cmd(pattern="lfy (.*)"))
+@borg.on(admin_cmd(pattern="ggl (.*)"))
 async def _(event):
     if event.fwd_from:
         return
