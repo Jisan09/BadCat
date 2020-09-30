077dcaffd583603e2c2d0de3587f347b67e3cb44
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-09-30 18:38:55 +0530

    Update fileconverts.py

--- userbot/plugins/fileconverts.py
+++ userbot/plugins/fileconverts.py
@@ -6,11 +6,59 @@ from io import BytesIO
 
 from telethon import types
 from telethon.errors import PhotoInvalidDimensionsError
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.messages import SendMediaRequest
 
 from .. import CMD_HELP
 from ..utils import admin_cmd, edit_or_reply, progress, sudo_cmd
+from . import unzip
 
+if not os.path.isdir("./temp"):
+    os.makedirs("./temp")
+
+
+@borg.on(admin_cmd(pattern="gif$"))
+async def _(event):
+    catreply = await event.get_reply_message()
+    if not catreply or not catreply.media or not catreply.media.document:
+        return await edit_or_reply(event, "`Stupid!, This is not animated sticker.`")
+    elif catreply.media.document.mime_type != "application/x-tgsticker":
+        return await edit_or_reply(event, "`Stupid!, This is not animated sticker.`")
+    reply_to_id = event.message
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    chat = "@tgstogifbot"
+    catevent = await edit_or_reply(event, "`Converting to gif ...`")
+    async with event.client.conversation(chat) as conv:
+        try:
+            await silently_send_message(conv, "/start")
+            await event.client.send_file(chat, catreply.media)
+            response = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+            if response.text.startswith("Send me an animated sticker!"):
+                return await catevent.edit("`This file is not supported`")
+            catresponse = response if response.media else await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+            catfile = await event.client.download_media(catresponse, "./temp")
+            catgif = await unzip(catfile)
+            await event.client.send_file(
+                event.chat_id,
+                catgif,
+                support_streaming=True,
+                force_document=False,
+                reply_to=reply_to_id,
+            )
+            await catevent.delete()
+        except YouBlockedUserError:
+            await catevent.edit("Unblock @tgstogifbot")
+            return
+
+
+async def silently_send_message(conv, text):
+    await conv.send_message(text)
+    response = await conv.get_response()
+    await conv.mark_read(message=response)
+    return response
 
 @borg.on(admin_cmd(pattern="stoi$"))
 @borg.on(sudo_cmd(pattern="stoi$", allow_sudo=True))
