6297cdc3cb524d37bfa4c6f94c8e64e1ebf73b52
Jisan09 <66010486+Jisan09@users.noreply.github.com>
2020-10-02 06:00:13 +0000

    pylint: auto fixes

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -91,7 +91,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
+            reply_pop_up_alert = (
+                "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
+            )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(
@@ -108,7 +110,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
+            reply_pop_up_alert = (
+                "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
+            )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.+?)_(.+)")))
@@ -119,9 +123,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             encrypted_tcxt = event.pattern_match.group(2)
             reply_pop_up_alert = encrypted_tcxt
         else:
-            reply_pop_up_alert = (
-                "You little shit, why are you looking at this ? Go away and do your own work,idiot"
-            )
+            reply_pop_up_alert = "You little shit, why are you looking at this ? Go away and do your own work,idiot"
 
         await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
@@ -161,7 +163,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                         caption=plugin_name,
                     )
         else:
-            reply_pop_up_alert = "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
+            reply_pop_up_alert = (
+                "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
+            )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
@@ -169,7 +173,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         if event.query.user_id == bot.uid:
             await event.edit("menu closed")
         else:
-            reply_pop_up_alert = "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
+            reply_pop_up_alert = (
+                "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
+            )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats")))
