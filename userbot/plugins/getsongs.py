5649f812cc84c37f2d6c2c9166fa54ecdad9adc0
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-04 20:07:09 +0530

    Update getsongs.py

--- userbot/plugins/getsongs.py
+++ userbot/plugins/getsongs.py
@@ -7,27 +7,18 @@ import os
 from pathlib import Path
 
 import pybase64
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from validators.url import url
 
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import (
-    ALIVE_NAME,
-    CMD_HELP,
-    name_dl,
-    runcmd,
-    song_dl,
-    thumb_dl,
-    video_dl,
-    yt_search,
-)
-
+from . import ALIVE_NAME, CMD_HELP, name_dl, runcmd, song_dl, thumb_dl, video_dl, yt_search
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 USERNAME = str(Config.LIVE_USERNAME) if Config.LIVE_USERNAME else "@Jisan7509"
 
-
-@borg.on(admin_cmd(pattern="(song|song320)($| (.*))"))
-@borg.on(sudo_cmd(pattern="(song|song320)($| (.*))", allow_sudo=True))
+@bot.on(admin_cmd(pattern="(song|song320)($| (.*))"))
+@bot.on(sudo_cmd(pattern="(song|song320)($| (.*))", allow_sudo=True))
 async def _(event):
     reply_to_id = None
     if event.from_id != bot.uid:
@@ -100,8 +91,8 @@ async def _(event):
             os.remove(files)
 
 
-@borg.on(admin_cmd(pattern="vsong( (.*)|$)"))
-@borg.on(sudo_cmd(pattern="vsong( (.*)|$)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="vsong( (.*)|$)"))
+@bot.on(sudo_cmd(pattern="vsong( (.*)|$)", allow_sudo=True))
 async def _(event):
     reply_to_id = None
     if event.from_id != bot.uid:
@@ -169,7 +160,7 @@ async def _(event):
         if files and os.path.exists(files):
             os.remove(files)
 
-
+            
 CMD_HELP.update(
     {
         "getsongs": "__**PLUGIN NAME :** Get Songs__\
