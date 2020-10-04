b5ce536505517da0a66490323ed995615388026f
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-04 18:36:10 +0530

    Update __init__.py

--- userbot/helpers/__init__.py
+++ userbot/helpers/__init__.py
@@ -3,5 +3,5 @@ from .aria2utils import *
 from .exceptions import CancelProcess
 from .functions import *
 from .memeifyhelpers import *
-from .progress import progress
+from .progress import *
 from .qhelper import process
