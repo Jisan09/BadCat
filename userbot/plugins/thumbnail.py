4f16923a52b742adf8e433672ab4ec3e86803423
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-04 18:14:03 +0530

    Update thumbnail.py

--- userbot/plugins/thumbnail.py
+++ userbot/plugins/thumbnail.py
@@ -1,54 +1,20 @@
 # Thumbnail Utilities ported from uniborg
 # credits @spechide
 
-import asyncio
 import os
-import time
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from PIL import Image
 
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import CMD_HELP
+from . import CMD_HELP, take_screen_shot
 
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
 
 
-async def get_video_thumb(video_file, output_directory=None, width=320):
-    # https://stackoverflow.com/a/13891070/4723940
-    out_put_file_name = output_directory + "/" + str(time.time()) + ".jpg"
-    metadata = extractMetadata(createParser(video_file))
-    ttl = 0
-    if metadata and metadata.has("duration"):
-        ttl = metadata.get("duration").seconds / 2
-    file_genertor_command = [
-        "ffmpeg",
-        "-ss",
-        str(ttl),
-        "-i",
-        video_file,
-        "-vframes",
-        "1",
-        out_put_file_name,
-    ]
-    # width = "90"
-    process = await asyncio.create_subprocess_exec(
-        *file_genertor_command,
-        # stdout must a pipe to be accessible as process.stdout
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    stderr.decode().strip()
-    stdout.decode().strip()
-    if os.path.lexists(out_put_file_name):
-        return out_put_file_name
-
-
-@borg.on(admin_cmd(pattern="savethumbnail$"))
-@borg.on(sudo_cmd(pattern="savethumbnail$", allow_sudo=True))
+@bot.on(admin_cmd(pattern="savethumb$"))
+@bot.on(sudo_cmd(pattern="savethumb$", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -60,23 +26,25 @@ async def _(event):
             await event.get_reply_message(), Config.TMP_DOWNLOAD_DIRECTORY
         )
         if downloaded_file_name.endswith(".mp4"):
-            downloaded_file_name = await get_video_thumb(
-                downloaded_file_name, Config.TMP_DOWNLOAD_DIRECTORY
+            metadata = extractMetadata(createParser(downloaded_file_name))
+            if metadata and metadata.has("duration"):
+                duration = metadata.get("duration").seconds
+            downloaded_file_name = await take_screen_shot(
+                downloaded_file_name, duration
             )
         # https://stackoverflow.com/a/21669827/4723940
         Image.open(downloaded_file_name).convert("RGB").save(thumb_image_path, "JPEG")
         # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
         os.remove(downloaded_file_name)
         await catevent.edit(
-            "Custom video / file thumbnail saved. "
-            + "This image will be used in the upload, till `.clearthumbnail`."
+            "Custom video/file thumbnail saved. This image will be used in the upload, till `.clearthumb`."
         )
     else:
         await catevent.edit("Reply to a photo to save custom thumbnail")
 
 
-@borg.on(admin_cmd(pattern="clearthumbnail$"))
-@borg.on(sudo_cmd(pattern="clearthumbnail$", allow_sudo=True))
+@bot.on(admin_cmd(pattern="clearthumb$"))
+@bot.on(sudo_cmd(pattern="clearthumb$", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -87,8 +55,8 @@ async def _(event):
     await edit_or_reply(event, "✅ Custom thumbnail cleared succesfully.")
 
 
-@borg.on(admin_cmd(pattern="getthumbnail$"))
-@borg.on(sudo_cmd(pattern="getthumbnail$", allow_sudo=True))
+@bot.on(admin_cmd(pattern="getthumb$"))
+@bot.on(sudo_cmd(pattern="getthumb$", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
