8caa276fe5f1b6f6bb3e6e711fc0ca5ad9fc1214
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-12 21:06:02 +0530

    Update clone.sh

--- .github/clone.sh
+++ .github/clone.sh
@@ -39,25 +39,10 @@ else
 fi
 
 FILE=/app/bin/
-
 if [ -d "$FILE" ] ; then
     echo "$FILE directory exists already."
 else
     bash ./.github/bins.sh
-
-
-if [ -d "$FILE" ] ; then
-    echo "$FILE directory exists already."
-else
-    mkdir /app/bin/
-    # downloading bins
-    wget -O /app/bin/megadown https://raw.githubusercontent.com/adekmaulana/megadown/master/megadown
-    wget -O /app/bin/cmrudl https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py
-    # changing bins permissions
-    chmod 755 bin/megadown
-    chmod 755 bin/cmrudl 
-    echo "Succesfully bins are added"
-
 fi
 
 python -m userbot
