a3c5cfa5671cea8a4c03f5f33597163812a090f4
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-14 02:42:28 +0530

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -10,22 +10,28 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 USERNAME = str(Config.LIVE_USERNAME) if Config.LIVE_USERNAME else "@Jisan7509"
 
 
-@borg.on(admin_cmd(pattern="help ?(.*)"))
+@bot.on(admin_cmd(pattern="help ?(.*)"))
 async def cmd_list(event):
     reply_to_id = None
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
     input_str = event.pattern_match.group(1)
     if input_str == "text":
-        string = ""
+        string = (
+            "Total {count} commands found in {plugincount} plugins of catuserbot\n\n"
+        )
+        catcount = 0
+        plugincount = 0
         for i in sorted(CMD_LIST):
-            string += "⚚" + i + "\n"
+            plugincount += 1
+            string += f"{plugincount}) Command found in Plugin " + i + " are \n"
             for iter_list in CMD_LIST[i]:
                 string += "    " + str(iter_list)
                 string += "\n"
+                catcount += 1
             string += "\n"
         if len(string) > 4095:
-            data = string
+            data = string.format(count=catcount, plugincount=plugincount)
             key = (
                 requests.post(
                     "https://nekobin.com/api/documents", json={"content": data}
@@ -38,15 +44,17 @@ async def cmd_list(event):
             reply_text = f"All commands of the catuserbot are [here]({url})"
             await event.edit(reply_text)
             return
-        await event.edit(string)
+        await event.edit(string.format(count=catcount, plugincount=plugincount))
         return
     if input_str:
         if input_str in CMD_LIST:
-            string = "**Commands found in plugin {}:**\n\n".format(input_str)
+            string = "**{count} Commands found in plugin {input_str}:**\n\n"
+            catcount = 0
             for i in CMD_LIST[input_str]:
                 string += f"  •  `{i}`"
                 string += "\n"
-            await event.edit(string)
+                catcount += 1
+            await event.edit(string.format(count=catcount, input_str=input_str))
         else:
             await event.edit(input_str + " is not a valid plugin!")
     else:
@@ -64,13 +72,13 @@ async def cmd_list(event):
         else:
             string = f"**Userbot Helper.. Provided by {DEFAULTUSER}\nUserbot Helper to reveal all the plugin names\n\n**Do `.help` plugin_name for commands\nDo `.info` plugin_name for usage\n\n"
             for i in sorted(CMD_LIST):
-                string += "◆`" + str(i)
-                string += "`   "
+                string += "◆```" + str(i)
+                string += "```   "
             await event.edit(string)
 
 
-@borg.on(admin_cmd(outgoing=True, pattern="info ?(.*)"))
-@borg.on(sudo_cmd(pattern="info ?(.*)", allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="info ?(.*)"))
+@bot.on(sudo_cmd(pattern="info ?(.*)", allow_sudo=True))
 async def info(event):
     """ For .info command,"""
     args = event.pattern_match.group(1).lower()
@@ -81,18 +89,21 @@ async def info(event):
             await edit_or_reply(event, "Please specify a valid plugin name.")
     else:
         string = "**Please specify which plugin do you want help for !!**\
+            \n**Number of plugins : **`{count}`\
             \n**Usage:** `.info` <plugin name>\n\n"
+        catcount = 0
         for i in sorted(CMD_HELP):
-            string += "◆`" + str(i)
-            string += "`   "
-        await edit_or_reply(event, string)
+            string += "◆" + f"`{str(i)}`"
+            string += "   "
+            catcount += 1
+        await edit_or_reply(event, string.format(count=catcount))
 
 
-@borg.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
+@bot.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
-    result = await borg(functions.help.GetNearestDcRequest())
+    result = await bot(functions.help.GetNearestDcRequest())
     result = (
         result.stringify()
         + "\n\nList Of Telegram Data Centres:\
@@ -106,26 +117,55 @@ async def _(event):
     await event.edit(result)
 
 
-@borg.on(sudo_cmd(allow_sudo=True, pattern="help(?: |$)(.*)"))
+@bot.on(sudo_cmd(allow_sudo=True, pattern="help(?: |$)(.*)"))
 async def info(event):
-    if event.fwd_from:
-        return
-    args = event.pattern_match.group(1).lower()
     input_str = event.pattern_match.group(1)
-    if args:
-        if args in SUDO_LIST:
-            if input_str in SUDO_LIST:
-                string = "**Commands found in plugin {}:**\n\n".format(input_str)
-                for i in CMD_LIST[input_str]:
-                    string += f"  •  `{i}`"
-                    string += "\n"
-                await event.reply(string)
+    if input_str == "text":
+        string = "Total {count} commands found in {plugincount} sudo plugins of catuserbot\n\n"
+        catcount = 0
+        plugincount = 0
+        for i in sorted(SUDO_LIST):
+            plugincount += 1
+            string += f"{plugincount}) Command found in Plugin " + i + " are \n"
+            for iter_list in SUDO_LIST[i]:
+                string += "    " + str(iter_list)
+                string += "\n"
+                catcount += 1
+            string += "\n"
+        if len(string) > 4095:
+            data = string.format(count=catcount, plugincount=plugincount)
+            key = (
+                requests.post(
+                    "https://nekobin.com/api/documents", json={"content": data}
+                )
+                .json()
+                .get("result")
+                .get("key")
+            )
+            url = f"https://nekobin.com/{key}"
+            reply_text = f"All commands of the catuserbot are [here]({url})"
+            await event.reply(reply_text)
+            return
+        await event.reply(string.format(count=catcount, plugincount=plugincount))
+        return
+    if input_str:
+        if input_str in SUDO_LIST:
+            string = "**{count} Commands found in plugin {input_str}:**\n\n"
+            catcount = 0
+            for i in SUDO_LIST[input_str]:
+                string += f"  •  `{i}`"
+                string += "\n"
+                catcount += 1
+            await event.reply(string.format(count=catcount, input_str=input_str))
         else:
-            await event.reply(args + " is not a valid plugin!")
+            await event.reply(input_str + " is not a valid plugin!")
     else:
         string = "**Please specify which plugin do you want help for !!**\
+            \n**Number of plugins : **`{count}`\
             \n**Usage:** `.help` <plugin name>\n\n"
+        catcount = 0
         for i in sorted(SUDO_LIST):
-            string += "◆`" + str(i)
-            string += "`   "
-        await event.reply(string)
+            string += "◆" + f"`{str(i)}`"
+            string += "   "
+            catcount += 1
+        await event.reply(string.format(count=catcount))
