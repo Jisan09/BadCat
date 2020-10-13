ed64646e818aed1ac0bb438ccf45fc7540e7fad4
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-13 20:09:24 +0530

    Update sangamata.py

--- userbot/plugins/sangamata.py
+++ userbot/plugins/sangamata.py
@@ -3,7 +3,7 @@ import asyncio
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import parse_pre, sanga_seperator
+from . import parse_pre, sanga_seperator,CMD_HELP
 
 
 @bot.on(admin_cmd(pattern="(sg|sgu)($| (.*))"))
