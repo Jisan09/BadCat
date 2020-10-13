e42728b6483e7245d3077259a1a04feb3a7566dc
Jisan09 <66010486+Jisan09@users.noreply.github.com>
2020-10-13 14:40:16 +0000

    pylint: auto fixes

--- userbot/plugins/sangamata.py
+++ userbot/plugins/sangamata.py
@@ -3,7 +3,7 @@ import asyncio
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import parse_pre, sanga_seperator,CMD_HELP
+from . import CMD_HELP, parse_pre, sanga_seperator
 
 
 @bot.on(admin_cmd(pattern="(sg|sgu)($| (.*))"))
