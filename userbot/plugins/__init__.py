239fa03a0d3d71704fd2e029d1f2f1f5b4cd92ec
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-13 19:31:52 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -21,8 +21,14 @@ if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "thumb_image.jpg"
 
 # thumb image
-with open(thumb_image_path, "wb") as f:
-    f.write(requests.get(Config.THUMB_IMAGE).content)
+if Config.THUMB_IMAGE is not None:
+    with open(thumb_image_path, "wb") as f:
+        f.write(requests.get(Config.THUMB_IMAGE).content)
+
+cat_users = [bot.uid]
+if Config.SUDO_USERS:
+    for user in Config.SUDO_USERS:
+        cat_users.append(user)
 
 
 def check(cat):
