7d6971736e2ffaaa0a4fc7f06f176cb5f6ab3056
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-14 11:25:53 +0530

    Update animation4.py

--- userbot/plugins/animation4.py
+++ userbot/plugins/animation4.py
@@ -5,7 +5,7 @@ import asyncio
 
 from userbot import ALIVE_NAME, utils
 
-from ..utils import admin_cmd
+from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 DEF = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 USR = str(Config.LIVE_USERNAME) if Config.LIVE_USERNAME else "@Jisan7509"
