160392cee26f6e7c2476070505404e7be4f74049
Jisan09 <66010486+Jisan09@users.noreply.github.com>
2020-10-14 18:22:06 +0000

    pylint: auto fixes

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -215,9 +215,7 @@ async def fetch_info(replied_user, event):
         if first_name
         else ("This User has no First Name")
     )
-    last_name = (
-        last_name.replace("\u2060", "") if last_name else (" ")
-    )
+    last_name = last_name.replace("\u2060", "") if last_name else (" ")
     username = "@{}".format(username) if username else ("This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
     caption = "<b><i>USER INFO from druv's database :</i></b>\n\n"
