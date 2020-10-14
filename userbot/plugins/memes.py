fea7576323afe254ef7a4bb9c1691edf52a82a50
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-14 15:31:08 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -4,6 +4,7 @@
 # Licensed under the Raphielscape Public License, Version 1.b (the "License");
 # you may not use this file except in compliance with the License.
 
+import asyncio
 import random
 import re
 
