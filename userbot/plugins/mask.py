396b07f7369a4c9ad85db60805f2f844dff95009
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-15 19:59:51 +0530

    Update mask.py

--- userbot/plugins/mask.py
+++ userbot/plugins/mask.py
@@ -270,8 +270,9 @@ async def catbot(catmemes):
 
 CMD_HELP.update(
     {
-        "mask": "`.mask` reply to any image file:\
-      \nUSAGE:makes an image a different style try out your own.\
-      "
+        "mask": "__**PLUGIN NAME :** Mask__\
+\n\n**📌 CMD ➥** `.mask` | `.awooify` | `.lolice` | `.bun` | `.iphx` \
+\n\n**USAGE   ➥  **Makes an image a different style try out your own.\
+"
     }
 )
