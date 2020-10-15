bb7a2dd9ce7c854fe7ea1d387cb4987bfd04c26f
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-15 17:51:50 +0530

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -1,16 +1,18 @@
+import asyncio
+
 import requests
 from telethon import functions
 
 from userbot import ALIVE_NAME
 
-from .. import CMD_HELP, CMD_LIST, SUDO_LIST
+from .. import CMD_HELP, CMD_LIST, SUDO_LIST, yaml_format
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 USERNAME = str(Config.LIVE_USERNAME) if Config.LIVE_USERNAME else "@Jisan7509"
 
 
-@bot.on(admin_cmd(pattern="help ?(.*)"))
+@bot.on(admin_cmd(outgoing=True, pattern="help ?(.*)"))
 async def cmd_list(event):
     reply_to_id = None
     if event.reply_to_msg_id:
@@ -24,7 +26,7 @@ async def cmd_list(event):
         plugincount = 0
         for i in sorted(CMD_LIST):
             plugincount += 1
-            string += f"{plugincount}) Command found in Plugin " + i + " are \n"
+            string += f"{plugincount}) Commands found in Plugin " + i + " are \n"
             for iter_list in CMD_LIST[i]:
                 string += "    " + str(iter_list)
                 string += "\n"
@@ -41,22 +43,26 @@ async def cmd_list(event):
                 .get("key")
             )
             url = f"https://nekobin.com/{key}"
-            reply_text = f"All commands of the catuserbot are [here]({url})"
+            reply_text = f"**All commands of the catuserbot can be seen [here]({url})**"
             await event.edit(reply_text)
             return
         await event.edit(string.format(count=catcount, plugincount=plugincount))
         return
     if input_str:
         if input_str in CMD_LIST:
-            string = "**{count} Commands found in plugin {input_str}:**\n\n"
+            string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
             catcount = 0
             for i in CMD_LIST[input_str]:
-                string += f"  •  `{i}`"
+                string += f"  ◆  <code>{i}</code>"
                 string += "\n"
                 catcount += 1
-            await event.edit(string.format(count=catcount, input_str=input_str))
+            await event.edit(
+                string.format(count=catcount, input_str=input_str), parse_mode="HTML"
+            )
         else:
             await event.edit(input_str + " is not a valid plugin!")
+            await asyncio.sleep(3)
+            await event.delete()
     else:
         if Config.HELP_INLINETYPE is None:
             help_string = f"Userbot Helper.. Provided by [{DEFAULTUSER}]({USERNAME})\
@@ -70,11 +76,15 @@ async def cmd_list(event):
             await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
             await event.delete()
         else:
-            string = f"**Userbot Helper.. Provided by {DEFAULTUSER}\nUserbot Helper to reveal all the plugin names\n\n**Do `.help` plugin_name for commands\nDo `.info` plugin_name for usage\n\n"
+            string = "<b>Please specify which plugin do you want help for !!\
+                \nNumber of plugins : </b><code>{count}</code>\
+                \n<b>Usage:</b> <code>.help</code> <plugin name>\n\n"
+            catcount = 0
             for i in sorted(CMD_LIST):
-                string += "◆```" + str(i)
-                string += "```   "
-            await event.edit(string)
+                string += "◆" + f"<code>{str(i)}</code>"
+                string += "   "
+                catcount += 1
+            await event.edit(string.format(count=catcount), parse_mode="HTML")
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="info ?(.*)"))
@@ -88,33 +98,18 @@ async def info(event):
         else:
             await edit_or_reply(event, "Please specify a valid plugin name.")
     else:
-        string = "**Please specify which plugin do you want help for !!**\
-            \n**Number of plugins : **`{count}`\
-            \n**Usage:** `.info` <plugin name>\n\n"
+        string = "<b>Please specify which plugin do you want help for !!\
+            \nNumber of plugins : </b><code>{count}</code>\
+            \n<b>Usage : </b><code>.info</code> <plugin name>\n\n"
         catcount = 0
         for i in sorted(CMD_HELP):
-            string += "◆" + f"`{str(i)}`"
+            string += "◆ " + f"<code>{str(i)}</code>"
             string += "   "
             catcount += 1
-        await edit_or_reply(event, string.format(count=catcount))
-
-
-@bot.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
-async def _(event):
-    if event.fwd_from:
-        return
-    result = await bot(functions.help.GetNearestDcRequest())
-    result = (
-        result.stringify()
-        + "\n\nList Of Telegram Data Centres:\
-                                    \nDC1 : Miami FL, USA\
-                                    \nDC2 : Amsterdam, NL\
-                                    \nDC3 : Miami FL, USA\
-                                    \nDC4 : Amsterdam, NL\
-                                    \nDC5 : Singapore, SG\
-                                    "
-    )
-    await event.edit(result)
+        if event.from_id in Config.SUDO_USERS:
+            await event.reply(string.format(count=catcount), parse_mode="HTML")
+        else:
+            await event.edit(string.format(count=catcount), parse_mode="HTML")
 
 
 @bot.on(sudo_cmd(allow_sudo=True, pattern="help(?: |$)(.*)"))
@@ -150,22 +145,47 @@ async def info(event):
         return
     if input_str:
         if input_str in SUDO_LIST:
-            string = "**{count} Commands found in plugin {input_str}:**\n\n"
+            string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
             catcount = 0
             for i in SUDO_LIST[input_str]:
-                string += f"  •  `{i}`"
+                string += f"  ◆  <code>{i}</code>"
                 string += "\n"
                 catcount += 1
-            await event.reply(string.format(count=catcount, input_str=input_str))
+            await event.reply(
+                string.format(count=catcount, input_str=input_str), parse_mode="HTML"
+            )
+            await asyncio.sleep(3)
+            await event.delete()
         else:
-            await event.reply(input_str + " is not a valid plugin!")
+            reply = await event.reply(input_str + " is not a valid plugin!")
+            await asyncio.sleep(3)
+            await event.delete()
+            await reply.delete()
     else:
-        string = "**Please specify which plugin do you want help for !!**\
-            \n**Number of plugins : **`{count}`\
-            \n**Usage:** `.help` <plugin name>\n\n"
+        string = "<b>Please specify which plugin do you want help for !!\
+            \nNumber of plugins : </b><code>{count}</code>\
+            \n<b>Usage:</b> <code>.help</code> <plugin name>\n\n"
         catcount = 0
         for i in sorted(SUDO_LIST):
-            string += "◆" + f"`{str(i)}`"
+            string += "◆" + f"<code>{str(i)}</code>"
             string += "   "
             catcount += 1
-        await event.reply(string.format(count=catcount))
+        await event.reply(string.format(count=catcount), parse_mode="HTML")
+
+
+@bot.on(admin_cmd(pattern="dc"))
+async def _(event):
+    if event.fwd_from:
+        return
+    result = await bot(functions.help.GetNearestDcRequest())
+    result = (
+        yaml_format(result)
+        + "\n\n**List Of Telegram Data Centres:**\
+                                    \nDC1 : Miami FL, USA\
+                                    \nDC2 : Amsterdam, NL\
+                                    \nDC3 : Miami FL, USA\
+                                    \nDC4 : Amsterdam, NL\
+                                    \nDC5 : Singapore, SG\
+                                    "
+    )
+    await event.edit(result)
