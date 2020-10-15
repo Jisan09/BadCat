ab9867991c063689ecc723f6e40659dc6547c04e
Jisan09 <66010486+Jisan09@users.noreply.github.com>
2020-10-15 10:17:23 +0000

    pylint: auto fixes

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -15,7 +15,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import ChannelParticipantsAdmins, MessageEntityMentionName
 
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import ALIVE_NAME, BOTLOG, BOTLOG_CHATID, CMD_HELP, catmemes
+from . import ALIVE_NAME, CMD_HELP, catmemes
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 
