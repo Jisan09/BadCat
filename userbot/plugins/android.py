76ece084d63efecc8d6d0ad3c6f6b509098cd2de
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-22 17:01:28 +0530

    Update android.py

--- userbot/plugins/android.py
+++ userbot/plugins/android.py
@@ -22,6 +22,7 @@ DEVICES_DATA = (
 @bot.on(admin_cmd(pattern=r"magisk"))
 @bot.on(sudo_cmd(pattern=r"magisk", allow_sudo=True))
 async def kakashi(magisk):
+    MAGISK = "https://telegra.ph/file/a4b88cbc8da9c562fdb44.jpg"
     """magisk latest releases"""
     releases = (
         "__**Latest Magisk Releases:**__\n\n"
@@ -29,7 +30,8 @@ async def kakashi(magisk):
         f"**Beta : **[APK v8.0.2](https://github.com/topjohnwu/Magisk/releases/download/manager-v8.0.2/MagiskManager-v8.0.2.apk) | [ZIP v21.0](https://github.com/topjohnwu/Magisk/releases/download/v21.0/Magisk-v21.0.zip) | [Uninstaller](https://github.com/topjohnwu/Magisk/releases/download/v21.0/Magisk-uninstaller-20201003.zip)\n"
         f"**Canary : **[APK v4e0a3f5e](https://github.com/topjohnwu/magisk_files/blob/canary/app-debug.apk) | [ZIP v4e0a3f5e](https://github.com/topjohnwu/magisk_files/blob/canary/magisk-debug.zip) | [Uninstaller](https://github.com/topjohnwu/magisk_files/blob/canary/magisk-uninstaller.zip)"
     )
-    await edit_or_reply(magisk, releases)
+    #await edit_or_reply(magisk, releases)
+    await borg.send_file(magisk.chat_id,MAGISK,caption=releases,link_preview=False,allow_cache=True,)
 
 
 @bot.on(admin_cmd(outgoing=True, pattern=r"device(?: |$)(\S*)"))
