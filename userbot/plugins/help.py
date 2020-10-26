fa79fbac41d9658a49f7118917604614990810c0
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-27 03:50:14 +0530

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -4,10 +4,7 @@ import requests
 from telethon import functions
 
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import ALIVE_NAME, CMD_HELP, CMD_LIST, SUDO_LIST, yaml_format
-
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
-USERNAME = str(Config.LIVE_USERNAME) if Config.LIVE_USERNAME else "@Jisan7509"
+from . import ALIVE_NAME, CMD_HELP, CMD_LIST, SUDO_LIST, yaml_format, mention
 
 HELPTYPE = Config.HELP_INLINETYPE or True
 
@@ -66,7 +63,7 @@ async def cmd_list(event):
             await event.delete()
     else:
         if HELPTYPE is True:
-            help_string = f"Userbot Helper.. Provided by [{DEFAULTUSER}]({USERNAME})\
+            help_string = f"Userbot Helper.. Provided by {mention}\
                           \nUserbot Helper to reveal all the plugin names\
                           \n__Do__ `.help` __plugin_name for commands, in case popup doesn't appear.__\
                           \nDo `.info` plugin_name for usage"
