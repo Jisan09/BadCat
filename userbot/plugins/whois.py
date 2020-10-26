8526dabb3aedd129765b0fd1b7ea84fe0f7805a6
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-26 12:26:51 +0530

    Update whois.py

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -1,11 +1,9 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
+# Userbot module for getiing info about any user on Telegram(including you!).
+
+# Copyright (C) 2019 The Raphielscape Company LLC.(who is from raphielscape)
+
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-#
-# The entire source code is OSSRPL except 'whois' which is MPL
-# License: MPL and OSSRPL
-""" Userbot module for getiing info about any user on Telegram(including you!). """
 
 import html
 import os
@@ -82,7 +80,7 @@ async def _(event):
         sw,
         cas,
     )
-    await event.edit(caption)
+    await edit_or_reply(event, caption)
 
 
 async def get_full_user(event):
@@ -126,20 +124,20 @@ async def get_full_user(event):
 @bot.on(admin_cmd(pattern="whois(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="whois(?: |$)(.*)", allow_sudo=True))
 async def who(event):
-    cat = await edit_or_reply(event, "`Fetching userinfo wait ...`")
+    cat = await edit_or_reply(event, "`Fetching userinfo wait...`")
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
     replied_user = await get_user(event)
     try:
         photo, caption = await fetch_info(replied_user, event)
     except AttributeError:
-        await edit_or_reply(event, "`Could not fetch info of that user.`")
+        await edit_or_reply(cat, "`Could not fetch info of that user.`")
         return
     message_id_to_reply = event.message.reply_to_msg_id
     if not message_id_to_reply:
         message_id_to_reply = None
     try:
-        await borg.send_file(
+        await event.client.send_file(
             event.chat_id,
             photo,
             caption=caption,
@@ -256,7 +254,7 @@ async def get_user_from_event(event):
     """ Get the user from argument or replied message. """
     args = event.pattern_match.group(1).split(":", 1)
     extra = None
-    if event.reply_to_msg_id and not len(args) == 2:
+    if event.reply_to_msg_id and len(args) != 2:
         previous_message = await event.get_reply_message()
         user_obj = await event.client.get_entity(previous_message.sender_id)
         extra = event.pattern_match.group(1)
