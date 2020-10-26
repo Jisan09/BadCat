aa1de559c06fc1939c1d013eb12e2596cef899b8
Jisan09 <66010486+Jisan09@users.noreply.github.com>
2020-10-26 22:28:10 +0000

    pylint: auto fixes

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -18,7 +18,6 @@ from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 from . import CMD_HELP, catmemes, mention
 
 
-
 async def get_user(event):
     # Get the user from argument or replied message.
     if event.reply_to_msg_id:
