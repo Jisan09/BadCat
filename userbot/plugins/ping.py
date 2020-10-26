9b153764a06215ee53c43cd9e1326bb5d6e10fbe
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-27 00:56:04 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -63,7 +63,7 @@ async def _(event):
     event = await edit_or_reply(
         event,
         "</b></i>☞ Pong!</b></i>",
-        parse_mode="html",
+        parsemode="html",
     )
     end = datetime.now()
     ms = (end - start).microseconds / 1000
