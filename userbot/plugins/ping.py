5efc1afd42ef3ec554c75ce66505958e642e73d3
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-10-26 23:39:20 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from .. import CMD_HELP, mention
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-USERNAME = bot.uid
+USERID = bot.uid
 
 @bot.on(admin_cmd(pattern=f"fping$", outgoing=True))
 async def _(event):
@@ -62,7 +62,7 @@ async def _(event):
     event = await edit_or_reply(event, "__**☞ Pong!__**")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await event.edit(f"__**☞ Pong!__**\n➥ {ms}\n➥ __**Bot**__ __**of**__ {mention}")
+    await event.edit(f"__**☞ Pong!__**\n➥ {ms}\n➥ __**Bot**__ __**of**__ {mention(USERID)}")
 
 
 CMD_HELP.update(
