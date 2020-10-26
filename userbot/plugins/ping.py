9eb8e5590f6708a4166d56def8fe044ad7b9eb64
Jisan09 <66010486+Jisan09@users.noreply.github.com>
2020-10-26 19:19:51 +0000

    pylint: auto fixes

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -60,7 +60,7 @@ async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    event = await edit_or_reply(event, "</b></i>☞ Pong!</b></i>",parse_mode="html")
+    event = await edit_or_reply(event, "</b></i>☞ Pong!</b></i>", parse_mode="html")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await event.edit(
