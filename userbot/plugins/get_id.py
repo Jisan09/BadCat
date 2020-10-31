5126c58794dd2210f69b31e6177ebedae3ab54bc
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-11-01 01:03:34 +0530

    Update get_id.py

--- userbot/plugins/get_id.py
+++ userbot/plugins/get_id.py
@@ -1,15 +1,35 @@
 from telethon.utils import pack_bot_file_id
 
-from .. import CMD_HELP
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
+from . import CMD_HELP
 
 
-@bot.on(admin_cmd(pattern="get_id"))
-@bot.on(sudo_cmd(pattern="get_id", allow_sudo=True))
+@bot.on(admin_cmd(pattern="(get_id|id)( (.*)|$)"))
+@bot.on(sudo_cmd(pattern="(get_id|id)( (.*)|$)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    if event.reply_to_msg_id:
+    input_str = event.pattern_match.group(2)
+    if input_str:
+        try:
+            p = await event.client.get_entity(input_str)
+        except Exception as e:
+            return await edit_delete(event, f"`{str(e)}`", 5)
+        try:
+            if p.first_name:
+                return await edit_or_reply(
+                    event, f"The id of the user `{input_str}` is `{p.id}`"
+                )
+        except:
+            try:
+                if p.title:
+                    return await edit_or_reply(
+                        event, f"The id of the chat/channel `{p.title}` is `{p.id}`"
+                    )
+            except:
+                pass
+        await edit_or_reply(event, "`Either give input as username or reply to user`")
+    elif event.reply_to_msg_id:
         await event.get_input_chat()
         r_msg = await event.get_reply_message()
         if r_msg.media:
@@ -34,7 +54,8 @@ async def _(event):
 CMD_HELP.update(
     {
         "get_id": "__**PLUGIN NAME :** Get_id__\
-    \n\n📌** CMD ➥** `.get_id` or `.get_id` <reply on a user msg>\
-    \n**USAGE   ➥  **To get id of certain chat, channel or user."
+    \n\n📌** CMD ➥** `.get_id` or `.id` <reply on a user msg>\
+    \n**USAGE   ➥  **__If given input then shows id of that given chat/channel/user else if you reply to user then shows id of the replied user \
+    along with current chat id and if not replied to user or given input then just show id of the chat where you used the command__"
     }
 )
