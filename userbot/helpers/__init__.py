ce0d72eff6aa4d1ab0e545797bceed9dcc68fb65
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-11-23 23:52:32 +0530

    Update __init__.py

--- userbot/helpers/__init__.py
+++ userbot/helpers/__init__.py
@@ -1,6 +1,5 @@
 from . import emojify, fonts
 from . import memeshelper as catmemes
-from .aria2utils import *
 from .exceptions import CancelProcess
 from .functions import *
 from .memeifyhelpers import *
