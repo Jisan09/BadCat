52b374df0b3493aed1943f6734d1313e610f129b
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-11-23 18:20:25 +0530

    Update groupdata.py

--- userbot/plugins/groupdata.py
+++ userbot/plugins/groupdata.py
@@ -32,7 +32,7 @@ from telethon.tl.types import (
 )
 from telethon.utils import get_input_location
 
-from .. import BOTLOG, BOTLOG_CHATID, CMD_HELP
+from . import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
