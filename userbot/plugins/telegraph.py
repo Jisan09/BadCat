5846e6d0a68dc690099a7a46434d3bafb9e59e15
Jisan09 <66010486+Jisan09@users.noreply.github.com>
2020-11-24 05:56:50 +0000

    pylint: auto fixes

--- userbot/plugins/telegraph.py
+++ userbot/plugins/telegraph.py
@@ -13,6 +13,7 @@ telegraph = Telegraph()
 r = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
 auth_url = r["auth_url"]
 
+
 @bot.on(admin_cmd(pattern="telegraph (media|text) ?(.*)"))
 @bot.on(sudo_cmd(pattern="telegraph (media|text) ?(.*)", allow_sudo=True))
 @bot.on(admin_cmd(pattern="tg(m|t) ?(.*)"))
