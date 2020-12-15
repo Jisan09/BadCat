2f50828ee36570771f242d6468d833a6d4928eef
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-12-15 14:44:27 +0530

    Update ytdl.py

--- userbot/plugins/ytdl.py
+++ userbot/plugins/ytdl.py
@@ -230,7 +230,7 @@ async def youtube_search(
 async def kakashi(event):
     if event.fwd_from:
         return
-    chat = "@allsaverbot"
+    chat = "@instadownloadingbot"
     link = event.pattern_match.group(1)
     if "www.instagram.com" not in link:
         await edit_or_reply(
@@ -244,13 +244,13 @@ async def kakashi(event):
             msg_start = await conv.send_message("/start")
             response = await conv.get_response()
             msg = await conv.send_message(link)
-            details = await conv.get_response()
-            await conv.get_response()
-            await conv.get_response()
+            #details = await conv.get_response()
+            #await conv.get_response()
+            #await conv.get_response()
             video = await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await catevent.edit("**Error:** `unblock` @allsaverbot `and retry!`")
+            await catevent.edit("**Error:** `unblock` @instadownloadingbot `and retry!`")
             return
         await catevent.delete()
         cat = await event.client.send_file(
