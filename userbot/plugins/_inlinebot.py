7c28b421d9f6dca50aa4fb3c94db386e7a261054
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2021-02-15 02:45:23 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -165,7 +165,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = TEXT_TO_DISPLAY_IN_HELP
+            reply_pop_up_alert = f"{TEXT_TO_DISPLAY_IN_HELP}"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(
@@ -182,7 +182,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = TEXT_TO_DISPLAY_IN_HELP
+            reply_pop_up_alert = f"{TEXT_TO_DISPLAY_IN_HELP}"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.*)")))
@@ -241,7 +241,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                         caption=plugin_name,
                     )
         else:
-            reply_pop_up_alert = TEXT_TO_DISPLAY_IN_HELP
+            reply_pop_up_alert = f"{TEXT_TO_DISPLAY_IN_HELP}"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
@@ -249,7 +249,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         if event.query.user_id == bot.uid:
             await event.edit("menu closed")
         else:
-            reply_pop_up_alert = TEXT_TO_DISPLAY_IN_HELP
+            reply_pop_up_alert = f"{TEXT_TO_DISPLAY_IN_HELP}"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats")))
