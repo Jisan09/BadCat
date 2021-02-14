f086a350e5bcfd470290603c54b14c541dc709c4
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2021-02-15 02:33:34 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -110,8 +110,9 @@ class Config(object):
     NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD = int(
         os.environ.get("NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD", 3)
     )
-    # emoji to be displayed in .help
+    # For customizing .help
     EMOJI_TO_DISPLAY_IN_HELP = os.environ.get("EMOJI_TO_DISPLAY_IN_HELP", " ")
+    TEXT_TO_DISPLAY_IN_HELP = os.environ.get("TEXT_TO_DISPLAY_IN_HELP", None)
     # specify command handler that should be used for the plugins
     # this should be a valid "regex" pattern
     COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", r"\.")
