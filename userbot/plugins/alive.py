e1fe3c1ddd10ff8ae4b9d98ff8deabe1a524e7cd
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2021-06-01 23:37:59 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -36,7 +36,7 @@ async def amireallyalive(event):
     EMOJI = "✧✧" if gvarstatus("ALIVE_EMOJI") is None else EMOJI = gvarstatus("ALIVE_EMOJI")
     CUSTOM_ALIVE_TEXT = "✮ MY BOT IS RUNNING SUCCESSFULLY ✮" if gvarstatus("ALIVE_TEXT") is None else CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
     if gvarstatus("ALIVE_PIC") is not None: CAT = [x for x in gvarstatus("ALIVE_PIC").split()]
-        CAT_IMG = list(CAT)
+    CAT_IMG = list(CAT)
     ##############################END#########################################
     if CAT_IMG:
         PIC = random.choice(CAT_IMG)
