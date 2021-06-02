d82d1f75788a2a6ce3ea70c017206e2f0f0ce383
Jisan09 <66010486+Jisan09@users.noreply.github.com>
2021-06-02 20:48:56 +0000

    pylint: auto fixes

--- userbot/plugins/antispambot.py
+++ userbot/plugins/antispambot.py
@@ -140,10 +140,10 @@ async def caschecker(event):
         text += banned_users
         if not cas_count:
             text = "No CAS Banned users found!"
-    except ChatAdminRequiredError as carerr:
+    except ChatAdminRequiredError:
         await catevent.edit("`CAS check failed: Admin privileges are required`")
         return
-    except BaseException as be:
+    except BaseException:
         await catevent.edit("`CAS check failed`")
         return
     await catevent.edit(text)
@@ -189,10 +189,10 @@ async def caschecker(event):
         text += banned_users
         if not cas_count:
             text = "No spamwatch Banned users found!"
-    except ChatAdminRequiredError as carerr:
+    except ChatAdminRequiredError:
         await catevent.edit("`spamwatch check failed: Admin privileges are required`")
         return
-    except BaseException as be:
+    except BaseException:
         await catevent.edit("`spamwatch check failed`")
         return
     await catevent.edit(text)
