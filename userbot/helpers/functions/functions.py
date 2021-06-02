1da5cad40a19a52b7a83b3a2c5e8e23b22de08a2
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-06-02 17:47:08 +0000

    pylint: auto fixes

--- userbot/helpers/functions/functions.py
+++ userbot/helpers/functions/functions.py
@@ -5,7 +5,7 @@ from textwrap import wrap
 from uuid import uuid4
 
 import requests
-from PIL import Image, ImageDraw, ImageFont,ImageColor
+from PIL import Image, ImageColor, ImageDraw, ImageFont
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from ...Config import Config
