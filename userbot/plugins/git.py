f50e142b80b6e571a5c1ab4af6c668633d82f2b4
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2021-06-12 01:55:57 +0530

    Update git.py

--- userbot/plugins/git.py
+++ userbot/plugins/git.py
@@ -33,7 +33,7 @@ async def source(e):
     "Source code link of userbot"
     await edit_or_reply(
         e,
-        "Click [here](https://github.com/sandy1709/catuserbot) to open this bot source code\
+        "Click [here](https://github.com/Jisan09/catuserbot) to open this bot source code\
         \nClick [here](https://github.com/Mr-confused/catpack) to open supported link for heroku",
     )
 
