4ad9491a3fed1f95398e9c242bef0bccc0abc595
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2021-06-11 19:26:15 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -41,10 +41,10 @@ async def amireallyalive(event):
         PIC = random.choice(A_IMG)
         cat_caption = f"**{ALIVE_TEXT}**\n\n"
         cat_caption += f"**{EMOJI} Master: {mention}**\n"
+        cat_caption += f"**{EMOJI} Uptime :** `{uptime}\n`"
         cat_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
         cat_caption += f"**{EMOJI} Catuserbot Version :** `{catversion}`\n"
-        cat_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
-        cat_caption += f"**{EMOJI} Uptime :** `{uptime}\n`"
+        cat_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`" 
         cat_caption += f"**{EMOJI} Database :** `{check_sgnirts}`\n"
         await event.client.send_file(
             event.chat_id,
@@ -59,10 +59,10 @@ async def amireallyalive(event):
             event,
             f"**{ALIVE_TEXT}**\n\n"
             f"**{EMOJI} Master: {mention}**\n"
+            f"**{EMOJI} Uptime :** `{uptime}\n`"
             f"**{EMOJI} Telethon Version :** `{version.__version__}\n`"
             f"**{EMOJI} Catuserbot Version :** `{catversion}`\n"
             f"**{EMOJI} Python Version :** `{python_version()}\n`"
-            f"**{EMOJI} Uptime :** `{uptime}\n`"
             f"**{EMOJI} Database :** `{check_sgnirts}`\n",
         )
 
