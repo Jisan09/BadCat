9277a4b16fd4efad65399b8912f52c8462257999
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2021-06-13 01:36:03 +0530

    Update format.py

--- userbot/helpers/utils/format.py
+++ userbot/helpers/utils/format.py
@@ -1,140 +1,87 @@
-import datetime
-import re
+import os
+from typing import Optional
 
-import requests
-from telethon.tl.tlobject import TLObject
-from telethon.tl.types import MessageEntityPre
-from telethon.utils import add_surrogate
+from moviepy.editor import VideoFileClip
+from PIL import Image
 
-from ..functions.utils import utc_to_local
+from ...core.logger import logging
+from ...core.managers import edit_or_reply
+from ..tools import media_type
+from .utils import runcmd
+
+LOGS = logging.getLogger(__name__)
 
 
-def paste_text(text, markdown=True):
-    if markdown:
-        asciich = ["**", "`", "__"]
-        for i in asciich:
-            text = re.sub(rf"\{i}", "", text)
-    try:
-        nekokey = (
-            requests.post("https://nekobin.com/api/documents", json={"content": text})
-            .json()
-            .get("result")
-            .get("key")
+async def media_to_pic(event, reply, noedits=False):  # sourcery no-metrics
+    mediatype = media_type(reply)
+    if mediatype not in [
+        "Photo",
+        "Round Video",
+        "Gif",
+        "Sticker",
+        "Video",
+        "Voice",
+        "Audio",
+        "Document",
+    ]:
+        return event, None
+    if not noedits:
+        catevent = await edit_or_reply(
+            event, f"`Transfiguration Time! Converting to ....`"
         )
-        link = f"https://nekobin.com/{nekokey}"
-    except Exception:
-        url = "https://del.dog/documents"
-        r = requests.post(url, data=text).json()
-        link = f"https://del.dog/{r['key']}"
-        if r["isUrl"]:
-            link = f"https://del.dog/v/{r['key']}"
-    return link
-
-
-def mentionuser(name, userid):
-    return f"[{name}](tg://user?id={userid})"
-
-
-def htmlmentionuser(name, userid):
-    return f"<a href='tg://user?id={userid}'>{name}</a>"
-
-
-# kanged from uniborg @spechide
-# https://github.com/SpEcHiDe/UniBorg/blob/d8b852ee9c29315a53fb27055e54df90d0197f0b/uniborg/utils.py#L250
-
-
-def reformattext(text):
-    return text.replace("~", "").replace("_", "").replace("*", "").replace("`", "")
-
-
-def replacetext(text):
-    return (
-        text.replace(
-            '"',
-            "",
-        )
-        .replace(
-            "\\r",
-            "",
-        )
-        .replace(
-            "\\n",
-            "",
-        )
-        .replace(
-            "\\",
-            "",
-        )
-    )
-
-
-def parse_pre(text):
-    text = text.strip()
-    return (
-        text,
-        [MessageEntityPre(offset=0, length=len(add_surrogate(text)), language="")],
-    )
-
-
-def yaml_format(obj, indent=0, max_str_len=256, max_byte_len=64):
-    """
-    Pretty formats the given object as a YAML string which is returned.
-    (based on TLObject.pretty_format)
-    """
-    result = []
-    if isinstance(obj, TLObject):
-        obj = obj.to_dict()
-
-    if isinstance(obj, dict):
-        if not obj:
-            return "dict:"
-        items = obj.items()
-        has_items = len(items) > 1
-        has_multiple_items = len(items) > 2
-        result.append(obj.get("_", "dict") + (":" if has_items else ""))
-        if has_multiple_items:
-            result.append("\n")
-            indent += 2
-        for k, v in items:
-            if k == "_" or v is None:
-                continue
-            formatted = yaml_format(v, indent)
-            if not formatted.strip():
-                continue
-            result.append(" " * (indent if has_multiple_items else 1))
-            result.append(f"{k}:")
-            if not formatted[0].isspace():
-                result.append(" ")
-            result.append(f"{formatted}")
-            result.append("\n")
-        if has_items:
-            result.pop()
-        if has_multiple_items:
-            indent -= 2
-    elif isinstance(obj, str):
-        # truncate long strings and display elipsis
-        result = repr(obj[:max_str_len])
-        if len(obj) > max_str_len:
-            result += "…"
-        return result
-    elif isinstance(obj, bytes):
-        # repr() bytes if it's printable, hex like "FF EE BB" otherwise
-        if all(0x20 <= c < 0x7F for c in obj):
-            return repr(obj)
-        return "<…>" if len(obj) > max_byte_len else " ".join(f"{b:02X}" for b in obj)
-    elif isinstance(obj, datetime.datetime):
-        # ISO-8601 without timezone offset (telethon dates are always UTC)
-        return utc_to_local(obj).strftime("%Y-%m-%d %H:%M:%S")
-    elif hasattr(obj, "__iter__"):
-        # display iterables one after another at the base indentation level
-        result.append("\n")
-        indent += 2
-        for x in obj:
-            result.append(f"{' ' * indent}- {yaml_format(x, indent + 2)}")
-            result.append("\n")
-        result.pop()
-        indent -= 2
     else:
-        return repr(obj)
+        catevent = event
+    catmedia = None
+    catfile = os.path.join("./temp/", "meme.png")
+    if os.path.exists(catfile):
+        os.remove(catfile)
+    if mediatype == "Photo":
+        catmedia = await reply.download_media(file="./temp")
+        im = Image.open(catmedia)
+        im.save(catfile)
+    elif mediatype in ["Audio", "Voice"]:
+        await event.client.download_media(reply, catfile, thumb=-1)
+    elif mediatype == "Sticker":
+        catmedia = await reply.download_media(file="./temp")
+        if catmedia.endswith(".tgs"):
+            catcmd = f"lottie_convert.py --frame 0 -if lottie -of png '{catmedia}' '{catfile}'"
+            stdout, stderr = (await runcmd(catcmd))[:2]
+            if stderr:
+                LOGS.info(stdout + stderr)
+        elif catmedia.endswith(".webp"):
+            im = Image.open(catmedia)
+            im.save(catfile)
+    elif mediatype in ["Round Video", "Video", "Gif"]:
+        await event.client.download_media(reply, catfile, thumb=-1)
+        if not os.path.exists(catfile):
+            catmedia = await reply.download_media(file="./temp")
+            clip = VideoFileClip(media)
+            try:
+                clip = clip.save_frame(catfile, 0.1)
+            except:
+                clip = clip.save_frame(catfile, 0)
+    elif mediatype == "Document":
+        mimetype = reply.document.mime_type
+        mtype = mimetype.split("/")
+        if mtype[0].lower() == "image":
+            catmedia = await reply.download_media(file="./temp")
+            im = Image.open(catmedia)
+            im.save(catfile)
+    if catmedia and os.path.lexists(catmedia):
+        os.remove(catmedia)
+    if os.path.lexists(catfile):
+        return catevent, catfile, mediatype
+    return catevent, None
 
-    return "".join(result)
+
+async def take_screen_shot(
+    video_file: str, duration: int, path: str = ""
+) -> Optional[str]:
+    thumb_image_path = path or os.path.join(
+        "./temp/", f"{os.path.basename(video_file)}.jpg"
+    )
+    command = f"ffmpeg -ss {duration} -i '{video_file}' -vframes 1 '{thumb_image_path}'"
+    err = (await runcmd(command))[1]
+    if err:
+        LOGS.error(err)
+    return thumb_image_path if os.path.exists(thumb_image_path) else None
