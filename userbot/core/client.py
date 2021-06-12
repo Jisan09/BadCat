76a9864a4fe187847624a5caeaf708e1df48f5a7
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2021-06-13 01:38:45 +0530

    Update client.py

--- userbot/core/client.py
+++ userbot/core/client.py
@@ -136,7 +136,7 @@ class CatUserBotClient(TelegramClient):
                         output = (await runcmd(command))[:2]
                         result = output[0] + output[1]
                         ftext += result
-                        pastelink = paste_text(ftext)
+                        pastelink = paste_text(ftext, markdown=False)
                         text = "**CatUserbot Error report**\n\n"
                         link = "[here](https://t.me/catuserbot_support)"
                         text += "If you wanna you can report it"
@@ -250,7 +250,7 @@ class CatUserBotClient(TelegramClient):
                         output = (await runcmd(command))[:2]
                         result = output[0] + output[1]
                         ftext += result
-                        pastelink = paste_text(ftext)
+                        pastelink = paste_text(ftext, markdown=False)
                         text = "**CatUserbot Error report**\n\n"
                         link = "[here](https://t.me/catuserbot_support)"
                         text += "If you wanna you can report it"
@@ -293,4 +293,4 @@ class CatUserBotClient(TelegramClient):
 CatUserBotClient.fast_download_file = download_file
 CatUserBotClient.fast_upload_file = upload_file
 CatUserBotClient.reload = restart_script
-CatUserBotClient.check_testcases = checking
+CatUserBotClient.check_testcases = 
