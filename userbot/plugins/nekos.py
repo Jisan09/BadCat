91c1039bf99eaccf3b622893bcc05c3a816eec73
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2021-06-15 00:28:21 +0530

    Update nekos.py

--- userbot/plugins/nekos.py
+++ userbot/plugins/nekos.py
@@ -30,7 +30,7 @@ async def _(event):
     reply_to = await reply_id(event)
     choose = event.pattern_match.group(1)
     if choose not in useless.hemtai:
-        return await edit_delete(event, f"Wrong catagory!! Choose from here:\n\n{useless.nsfw(useless.hemtai)}")
+        return await edit_delete(event, f"Wrong catagory!! Choose from here:\n\n{useless.nsfw(useless.hemtai)}",60)
     if await age_verification(event, reply_to):
         return
     catevent = await edit_or_reply(event, "`Processing Nekos...`")
