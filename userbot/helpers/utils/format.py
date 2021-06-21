47c105a4867f15200bd8732405ea451c94e1646b
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2021-06-21 20:25:29 +0530

    Update format.py

--- userbot/helpers/utils/format.py
+++ userbot/helpers/utils/format.py
@@ -10,13 +10,15 @@ from ..functions.utils import utc_to_local
 from .paste import pastetext
 
 
-async def paste_message(text, markdown=True):
+async def paste_message(text, pastetype="p", extension=None, markdown=True):
     if markdown:
         asciich = ["**", "`", "__"]
         for i in asciich:
             text = re.sub(rf"\{i}", "", text)
-    response = await pastetext(text)
-    return response["url"]
+    response = await pastetext(text, pastetype, extension)
+    if "url" in response:
+        return response["url"]
+    return "Error while pasting text to site"
 
 
 def paste_text(text, markdown=True):
@@ -88,6 +90,7 @@ def parse_pre(text):
 
 
 def yaml_format(obj, indent=0, max_str_len=256, max_byte_len=64):
+    # sourcery no-metrics
     """
     Pretty formats the given object as a YAML string which is returned.
     (based on TLObject.pretty_format)
