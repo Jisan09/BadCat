4eef9a0183f6d971f31d5ab59a990ce6cbbe0bd0
Jisan09 <66010486+Jisan09@users.noreply.github.com>
2021-06-24 15:00:59 +0000

    pylint: auto fixes

--- userbot/plugins/romance.py
+++ userbot/plugins/romance.py
@@ -1,15 +1,16 @@
-#Created by @Jisan7509
+# Created by @Jisan7509
 
 import base64
 import random
-from . import catub
+
 from telethon import functions, types
-from ..helpers.utils import _catutils, reply_id
-from ..core.managers import edit_delete,edit_or_reply
-from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.errors.rpcerrorlist import UserNotParticipantError
+from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
+from ..core.managers import edit_delete, edit_or_reply
+from ..helpers.utils import _catutils, reply_id
+from . import catub
 
 plugin_category = "useless"
 
@@ -29,32 +30,42 @@ async def some(event):
     """Its useless for single like you. Get a lover first"""
     inpt = event.pattern_match.group(1)
     reply_to_id = await reply_id(event)
-    count = 1 if not inpt else int(inpt) 
-    if count< 0 and count> 20:
-        await edit_delete(event,"`Give value in range 1-20`")
-    res = base64.b64decode("aHR0cHM6Ly90Lm1lL2pvaW5jaGF0L0NtZEEwVzYtSVVsbFpUUTk=").decode('utf-8')
+    count = 1 if not inpt else int(inpt)
+    if count < 0 and count > 20:
+        await edit_delete(event, "`Give value in range 1-20`")
+    res = base64.b64decode(
+        "aHR0cHM6Ly90Lm1lL2pvaW5jaGF0L0NtZEEwVzYtSVVsbFpUUTk="
+    ).decode("utf-8")
     resource = await event.client(GetFullChannelRequest(res))
     chat = resource.chats[0].username
     try:
-        await event.client(functions.channels.GetParticipantRequest(channel = chat, participant = event.from_id.user_id))
+        await event.client(
+            functions.channels.GetParticipantRequest(
+                channel=chat, participant=event.from_id.user_id
+            )
+        )
     except UserNotParticipantError:
         await event.client(Get(res.split("/")[4]))
         await event.client.edit_folder(resource.full_chat.id, 1)
-        await event.client(functions.account.UpdateNotifySettingsRequest(
-            peer= chat,
-            settings = types.InputPeerNotifySettings(
-                show_previews=False,
-                silent= True,
+        await event.client(
+            functions.account.UpdateNotifySettingsRequest(
+                peer=chat,
+                settings=types.InputPeerNotifySettings(
+                    show_previews=False,
+                    silent=True,
+                ),
             )
-        ))
-    catevent = await edit_or_reply(event,"`Wait babe...`😘")
+        )
+    catevent = await edit_or_reply(event, "`Wait babe...`😘")
     maxmsg = await event.client.get_messages(chat)
     start = random.randint(31, maxmsg.total)
-    if start > maxmsg.total-40:
-        start = maxmsg.total-40
+    if start > maxmsg.total - 40:
+        start = maxmsg.total - 40
     end = start + 41
     kiss = []
-    async for x in event.client.iter_messages(chat, min_id = start, max_id = end, reverse = True):
+    async for x in event.client.iter_messages(
+        chat, min_id=start, max_id=end, reverse=True
+    ):
         try:
             if x.media and x.media.document.mime_type == "video/mp4":
                 link = f"{res.split('j')[0]}{chat}/{x.id}"
@@ -63,7 +74,6 @@ async def some(event):
             pass
     kisss = random.sample(kiss, count)
     for i in kisss:
-        nood = await event.client.send_file(event.chat_id,i,reply_to=reply_to_id)
+        nood = await event.client.send_file(event.chat_id, i, reply_to=reply_to_id)
         await _catutils.unsavegif(event, nood)
     await catevent.delete()
-    
