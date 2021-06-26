60ecd8c4f559c8550f820708c1b1745957f90bb8
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2021-06-26 07:25:31 -0700

    Update events.py

--- userbot/core/events.py
+++ userbot/core/events.py
@@ -1,6 +1,7 @@
 import typing
 
 from telethon import events, functions, hints, types
+from telethon.tl.types import InputPeerChannel, InputPeerChat, InputPeerUser
 
 from ..Config import Config
 from .managers import edit_or_reply
@@ -79,6 +80,49 @@ class MessageEdited(NewMessage):
         pass
 
 
+async def safe_check_text(msg):  # sourcery no-metrics
+    if not msg:
+        return False
+    msg = str(msg)
+    return bool(
+        (
+            (Config.STRING_SESSION in msg)
+            or (str(Config.APP_ID) in msg)
+            or (Config.API_HASH in msg)
+            or (Config.TG_BOT_TOKEN in msg)
+            or (Config.HEROKU_API_KEY and Config.HEROKU_API_KEY in msg)
+            or (Config.OPEN_WEATHER_MAP_APPID and Config.OPEN_WEATHER_MAP_APPID in msg)
+            or (Config.IBM_WATSON_CRED_URL and Config.IBM_WATSON_CRED_URL in msg)
+            or (Config.OCR_SPACE_API_KEY and Config.OCR_SPACE_API_KEY in msg)
+            or (Config.GENIUS_API_TOKEN and Config.GENIUS_API_TOKEN in msg)
+            or (Config.REM_BG_API_KEY and Config.REM_BG_API_KEY in msg)
+            or (Config.CURRENCY_API and Config.CURRENCY_API in msg)
+            or (Config.G_DRIVE_CLIENT_ID and Config.G_DRIVE_CLIENT_ID in msg)
+            or (Config.G_DRIVE_CLIENT_SECRET and Config.G_DRIVE_CLIENT_SECRET in msg)
+            or (Config.G_DRIVE_DATA and Config.G_DRIVE_DATA in msg)
+            or (Config.LASTFM_API and Config.LASTFM_API in msg)
+            or (Config.LASTFM_SECRET and Config.LASTFM_SECRET in msg)
+            or (Config.LASTFM_PASSWORD_PLAIN and Config.LASTFM_PASSWORD_PLAIN in msg)
+            or (Config.SPAMWATCH_API and Config.SPAMWATCH_API in msg)
+            or (Config.RANDOM_STUFF_API_KEY and Config.RANDOM_STUFF_API_KEY in msg)
+            or (Config.GITHUB_ACCESS_TOKEN and Config.GITHUB_ACCESS_TOKEN in msg)
+            or (Config.DEEP_AI and Config.DEEP_AI in msg)
+            or (
+                Config.SCREEN_SHOT_LAYER_ACCESS_KEY
+                and Config.SCREEN_SHOT_LAYER_ACCESS_KEY in msg
+            )
+            or (
+                Config.IBM_WATSON_CRED_PASSWORD
+                and Config.IBM_WATSON_CRED_PASSWORD in msg
+            )
+            or (
+                Config.TG_2STEP_VERIFICATION_CODE
+                and Config.TG_2STEP_VERIFICATION_CODE in msg
+            )
+        )
+    )
+
+
 async def send_message(
     client,
     entity: "hints.EntityLike",
@@ -94,7 +138,7 @@ async def send_message(
     buttons: "hints.MarkupLike" = None,
     silent: bool = None,
     schedule: "hints.DateLike" = None,
-    comment_to: "typing.Union[int, types.Message]" = None
+    comment_to: "typing.Union[int, types.Message]" = None,
 ):
     chatid = entity
     if str(chatid) == str(Config.BOTLOG_CHATID):
@@ -113,21 +157,11 @@ async def send_message(
             schedule=schedule,
             comment_to=comment_to,
         )
-
     msg = message
-    if msg and (
-        (Config.STRING_SESSION in msg)
-        or (str(Config.APP_ID) in msg)
-        or (Config.API_HASH in msg)
-        or (Config.TG_BOT_TOKEN in msg)
-        or (Config.HEROKU_API_KEY and Config.HEROKU_API_KEY in msg)
-        or (
-            Config.SCREEN_SHOT_LAYER_ACCESS_KEY
-            and Config.SCREEN_SHOT_LAYER_ACCESS_KEY in msg
-        )
-    ):
+    safecheck = await safe_check_text(msg)
+    if safecheck:
         if Config.BOTLOG:
-            await client.sendmessage(
+            response = await client.sendmessage(
                 entity=Config.BOTLOG_CHATID,
                 message=msg,
                 reply_to=reply_to,
@@ -142,8 +176,8 @@ async def send_message(
                 schedule=schedule,
                 comment_to=comment_to,
             )
-
-        msg = "__Sorry I can't send this information in public chats i will send it in Bot Log group check it from there.__"
+        msglink = await client.get_msg_link(response)
+        msg = f"__Sorry I can't send this message in public chats it may have some sensitive data So check in __[Bot log group]({msglink})."
         return await client.sendmessage(
             entity=chatid,
             message=msg,
@@ -199,7 +233,7 @@ async def send_file(
     supports_streaming: bool = False,
     schedule: "hints.DateLike" = None,
     comment_to: "typing.Union[int, types.Message]" = None,
-    **kwargs
+    **kwargs,
 ):
     chatid = entity
     if str(chatid) == str(Config.BOTLOG_CHATID):
@@ -228,19 +262,10 @@ async def send_file(
         )
 
     msg = caption
-    if msg and (
-        (Config.STRING_SESSION in msg)
-        or (str(Config.APP_ID) in msg)
-        or (Config.API_HASH in msg)
-        or (Config.TG_BOT_TOKEN in msg)
-        or (Config.HEROKU_API_KEY and Config.HEROKU_API_KEY in msg)
-        or (
-            Config.SCREEN_SHOT_LAYER_ACCESS_KEY
-            and Config.SCREEN_SHOT_LAYER_ACCESS_KEY in msg
-        )
-    ):
+    safecheck = await safe_check_text(msg)
+    if safecheck:
         if Config.BOTLOG:
-            await client.sendfile(
+            response = await client.sendfile(
                 entity=Config.BOTLOG_CHATID,
                 file=file,
                 caption=msg,
@@ -263,8 +288,8 @@ async def send_file(
                 comment_to=comment_to,
                 **kwargs,
             )
-
-        msg = "__Sorry I can't send this information in public chats i will send it in Bot Log group check it from there__"
+        msglink = await client.get_msg_link(response)
+        msg = f"__Sorry I can't send this message in public chats it may have some sensitive data So check in __[Bot log group]({msglink})."
         return await client.sendfile(
             entity=chatid,
             file=file,
@@ -325,10 +350,18 @@ async def edit_message(
     file: "hints.FileLike" = None,
     force_document: bool = False,
     buttons: "hints.MarkupLike" = None,
-    schedule: "hints.DateLike" = None
+    schedule: "hints.DateLike" = None,
 ):
     chatid = entity
-    if str(chatid) == str(Config.BOTLOG_CHATID):
+    if isinstance(chatid, InputPeerChannel):
+        chat_id = int("-100" + str(chatid.channel_id))
+    elif isinstance(chatid, InputPeerChat):
+        chat_id = int("-" + str(chatid.chat_id))
+    elif isinstance(chatid, InputPeerUser):
+        chat_id = int(chatid.user_id)
+    else:
+        chat_id = chatid
+    if str(chat_id) == str(Config.BOTLOG_CHATID):
         return await client.editmessage(
             entity=entity,
             message=message,
@@ -342,19 +375,10 @@ async def edit_message(
             schedule=schedule,
         )
     main_msg = text
-    if main_msg and (
-        (Config.STRING_SESSION in main_msg)
-        or (str(Config.APP_ID) in main_msg)
-        or (Config.API_HASH in main_msg)
-        or (Config.TG_BOT_TOKEN in main_msg)
-        or (Config.HEROKU_API_KEY and Config.HEROKU_API_KEY in main_msg)
-        or (
-            Config.SCREEN_SHOT_LAYER_ACCESS_KEY
-            and Config.SCREEN_SHOT_LAYER_ACCESS_KEY in main_msg
-        )
-    ):
+    safecheck = await safe_check_text(main_msg)
+    if safecheck:
         if Config.BOTLOG:
-            await client.sendmessage(
+            response = await client.sendmessage(
                 entity=Config.BOTLOG_CHATID,
                 message=main_msg,
                 parse_mode=parse_mode,
@@ -365,7 +389,8 @@ async def edit_message(
                 buttons=buttons,
                 schedule=schedule,
             )
-        msg = "__Sorry I can't send this information in public chats i will send it in Bot Log group check it from there__"
+        msglink = await client.get_msg_link(response)
+        msg = f"__Sorry I can't send this message in public chats it may have some sensitive data So check in __[Bot log group]({msglink})."
         return await client.editmessage(
             entity=chatid,
             message=message,
