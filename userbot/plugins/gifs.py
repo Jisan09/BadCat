52576799eadde01ff0612d6a7f34915a14a5f9c0
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2021-06-29 06:53:44 -0700

    Update and rename romance.py to gifs.py

--- userbot/plugins/gifs.py
+++ userbot/plugins/gifs.py
@@ -2,7 +2,7 @@
 
 import base64
 import random
-
+import requests
 from telethon import functions, types
 from telethon.errors.rpcerrorlist import UserNotParticipantError
 from telethon.tl.functions.channels import GetFullChannelRequest
@@ -13,6 +13,46 @@ from ..helpers.utils import _catutils, reply_id
 from . import catub
 
 plugin_category = "useless"
+
+
+@catub.cat_cmd(
+    pattern="gifs(?:\s|$)([\s\S]*)",
+    command=("gifs", plugin_category),
+    info={
+        "header": "Sends random gifs",
+        "usage": "Search and send your desire gif randomly and in bulk",
+        "examples": [
+            "{tr}gifs cat",
+            "{tr}gifs cat ; <1-20>",
+        ],
+    },
+)
+async def some(event):
+    """Sends random gifs of your query"""
+    inpt = event.pattern_match.group(1)
+    reply_to_id = await reply_id(event)
+    if not inpt:
+        await edit_delete(event, "`Give an input to search...`")
+    count = 1
+    if ";" in inpt:
+        inpt,count = inpt.split(";")
+    if int(count) < 0 and int(count) > 20:
+        await edit_delete(event, "`Give value in range 1-20`")
+    catevent = await edit_or_reply(event, "`Sending gif....`")
+    res = requests.get('https://giphy.com/')
+    res = res.text.split('GIPHY_FE_WEB_API_KEY =')[1].split('\n')[0]
+    api_key = res[2:-1]
+    list_id = []
+    r = requests.get(f"https://api.giphy.com/v1/gifs/search?q={inpt}&api_key={api_key}&limit=100").json()
+    i = 0
+    while i < len(r["data"]):
+        list_id.append(r["data"][i]["id"])
+        i+=1
+    rlist = random.sample(list_id, int(count))
+    for items in rlist:
+        nood = await event.client.send_file(event.chat_id,f"https://media.giphy.com/media/{items}/giphy.gif", reply_to=reply_to_id)
+        await _catutils.unsavegif(event, nood)
+    await catevent.delete()
 
 
 @catub.cat_cmd(
