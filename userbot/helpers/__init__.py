5878b387c182b99f68dcf5cbf4d990868fde75cf
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2021-08-25 14:13:32 -0700

    Update __init__.py

--- userbot/helpers/__init__.py
+++ userbot/helpers/__init__.py
@@ -8,7 +8,6 @@ check = 0
 while flag:
     try:
         from . import nsfw as useless
-        from .chatbot import *
         from .functions import *
         from .memeifyhelpers import *
         from .progress import *
