774e85d0b0bc7d4a7e4b76203d84943385d43dd8
deepaiims <87997952+deepaiims@users.noreply.github.com>
2021-09-02 19:37:25 +0530

    Add files via upload

---
+++ userbot/plugins/a.py
@@ -0,0 +1,33 @@
+import random
+from userbot import catub
+
+from ..core.managers import edit_delete
+from ..helpers.utils import reply_id
+
+plugin_category = "utils"
+
+
+@catub.cat_cmd(
+    pattern="mcq ?(.*)",
+    command=("mcq", plugin_category),
+    info={
+        "header": "Chooses a random values in the given options, give a space to add multiple option",
+        "usage": [
+            "{tr}mcq <options>",
+            "{tr}mcq a b c d",
+            "{tr}mcq cat dog life death"
+        ],
+    },
+)
+async def Gay(event):
+    "Jai matadi"
+    if event.fwd_from:
+        return
+    inp = event.pattern_match.group(1)
+    reply_to_id = await reply_id(event)
+    if not inp:
+        return await edit_delete(
+            event, "What to choose from", 15
+        )
+    options = inp.split()
+    await edit_delete(event, random.choice(options), 120)
