44aeb6421e101419b8790bba158fc62ea17a590e
VINU <87769322+VinuXD@users.noreply.github.com>
2021-09-10 19:34:44 +0530

    Small change

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -109,11 +109,7 @@ async def bot_start(event):
                         \n\nPowered by [Catuserbot](https://t.me/catuserbot)"
         buttons = [
             (
-                Button.url("Repo", "https://github.com/sandy1709/catuserbot"),
-                Button.url(
-                    "Deploy",
-                    "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack&template=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack",
-                ),
+                Button.url("OWNER", "https://t.me/VinuXD"),
             )
         ]
     else:
