d32f8ba79b1a3e4c643ffb6306d12668ec062cd2
ADITYA PRASAD.S <70315903+adityaprasad502@users.noreply.github.com>
2021-09-11 19:28:13 +0530

    new commit

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -18,20 +18,23 @@ plugin_category = "tools"
 )
 async def _(event):
     "To check ping"
-    catevent = await edit_or_reply(event, "__**Hmm, Pinging....**__")
     flag = event.pattern_match.group(1)
     start = datetime.now()
     if flag == " -a":
+        catevent = await edit_or_reply(event, "__**Hmm, Pinging...**__")
         await catevent.edit("`!....`")
-        await asyncio.sleep(0.3)
+        await asyncio.sleep(0.1)
         await catevent.edit("`..!..`")
-        await asyncio.sleep(0.3)
+        await asyncio.sleep(0.1)
         await catevent.edit("`....!`")
         end = datetime.now()
         tms = (end - start).microseconds / 1000
-        ms = round((tms - 0.6) / 3, 3)
-        await catevent.edit(f"**⌘ Average Ping**:-\n **{ms} ms**")
+        ms = round((tms - 0.2) / 3, 3)
+        await catevent.edit(
+        f"┏━━━━━━━━━━━┓\n┃ ⁭⁫⁭<b><i>〣 Average Pong!</b></i>\n┃⁭⁫⁭<b><i> ⁭⁫⁭⁭〣 {ms}</b></i>\n┃⁭⁫⁭<b><i> ⁭〣 {hmention}</b></i>\n┗━━━━━━━━━━━┛"
+        , parse_mode="html")
     else:
+        catevent = await edit_or_reply(event, "__**Hmm, Pinging...**__")
         end = datetime.now()
         ms = (end - start).microseconds / 1000
         await catevent.edit(f"<b><i>〣 Ping: {ms} ms\n〣 Owner: {hmention}</b></i>", parse_mode="html")
