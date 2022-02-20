8d594b180089594fb620dc28a3268ac598a66f92
Sourcery AI <>
2022-02-20 19:12:57 +0000

    'Refactored by Sourcery'

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -655,7 +655,6 @@ async def pussycat(args):
     else:
         await edit_delete(args, "`I can't convert that...`")
         return
-    cmd = "/newvideo"
     packname = f"BadCat_{userid}_temp_pack"
     response = urllib.request.urlopen(
         urllib.request.Request(f"http://t.me/addstickers/{packname}")
@@ -665,6 +664,7 @@ async def pussycat(args):
         "  A <strong>Telegram</strong> user has created the <strong>Sticker&nbsp;Set</strong>."
         not in htmlstr
     ):
+        cmd = "/newvideo"
         async with args.client.conversation("@Stickers") as xconv:
             await delpack(
                 catevent,
